11c93af7655177396cf3cb6b35b1dcb6fe1bc27c
sjswuitchik <sjswuit@g.harvard.edu>
2020-02-06 12:22:17 -0500

    additional options added to repeatmasker commands when re-run on ordered and oriented chromosome fastas

--- 01_assemblies/code/repeatmasker.sh
+++ 01_assemblies/code/repeatmasker.sh
@@ -1,10 +1,7 @@
-module load RepeatMasker/4.0.5-fasrc05
+nohup RepeatMasker -species chicken -xsmall -gff -dir hetAtr_RM/ BPBGC_Hatr1.0.chromosomal_ordered_oriented.fasta &> nohup_hetAtr_RM.out&
 
-nohup RepeatMasker -species chicken -gff -dir hetAtr_RM/ hetAtr.1.fasta &> nohup_hetAtr_RM.out& 
+nohup RepeatMasker -species chicken -xsmall -gff -dir netAur_RM/ BPBGC_Naur1.0.chromosomal_ordered_oriented.fasta &> nohup_netAur_RM.out& 
 
-nohup RepeatMasker -species chicken -gff -dir netAur_RM/ netAur.1.fasta &> nohup_netAur_RM.out& 
+nohup RepeatMasker -species chicken -xsmall -gff -dir oxyJam_RM/ BPBGC_Ojam1.0.chromosomal_ordered_oriented.fasta &> nohup_oxyJam_RM.out&
 
-nohup RepeatMasker -species chicken -gff -dir oxyJam_RM/ oxyJam.1.fasta.gz &> nohup_oxyJam_RM.out& 
-
-nohup RepeatMasker -species chicken -gff -dir stiNae_RM/ stiNae.1.fasta.gz &> nohup_stiNae_RM.out& 
-
+nohup RepeatMasker -species chicken -xsmall -gff -dir stiNae_RM_chrom/ BPBGC_Snae1.0.chromosomal_ordered_oriented.fasta &> nohup_stiNae_RM.out&
