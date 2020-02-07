1bc4168ecb079f296678c3993bbcedf97f6b1ddb
sjswuitchik <sjswuit@g.harvard.edu>
2020-02-07 09:14:20 -0500

    reorganization of scripts into single script (pipeline.sh)

--- repeatmasker.sh
+++
@@ -1,7 +0,0 @@
-nohup RepeatMasker -species chicken -xsmall -gff -dir hetAtr_RM/ BPBGC_Hatr1.0.chromosomal_ordered_oriented.fasta &> nohup_hetAtr_RM.out&
-
-nohup RepeatMasker -species chicken -xsmall -gff -dir netAur_RM/ BPBGC_Naur1.0.chromosomal_ordered_oriented.fasta &> nohup_netAur_RM.out& 
-
-nohup RepeatMasker -species chicken -xsmall -gff -dir oxyJam_RM/ BPBGC_Ojam1.0.chromosomal_ordered_oriented.fasta &> nohup_oxyJam_RM.out&
-
-nohup RepeatMasker -species chicken -xsmall -gff -dir stiNae_RM_chrom/ BPBGC_Snae1.0.chromosomal_ordered_oriented.fasta &> nohup_stiNae_RM.out&
