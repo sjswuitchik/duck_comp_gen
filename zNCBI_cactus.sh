75892a203f2cc4b4a0d2b75c53421fc84f4e012c
sjswuitchik <sjswuit@g.harvard.edu>
2020-04-17 11:22:39 -0400

    Added notes for re-doing analyses with NCBI submissions

---
+++ zNCBI_cactus.sh
@@ -0,0 +1,36 @@
+##########################################
+## Re-doing ducks with NCBI submissions ##
+##########################################
+
+# /n/holyscratch01/informatics/swuitchik/ducks_project/cactus_ncbi 
+# FASTAs are already soft masked, so starting from CACTUS
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/075/105/GCA_011075105.1_BPBGC_Hatr_1.0/GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/074/415/GCA_011074415.1_BPBGC_Snae_1.0/GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/077/185/GCA_011077185.1_BPBGC_Ojam_1.0/GCA_011077185.1_BPBGC_Ojam_1.0_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/076/525/GCA_011076525.1_BPBGC_Naur_1.0/GCA_011076525.1_BPBGC_Naur_1.0_genomic.fna.gz
+ 
+mv GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz hetAtr.ncbi.fasta.gz
+mv GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna.gz stiNae.ncbi.fasta.gz
+mv GCA_011077185.1_BPBGC_Ojam_1.0_genomic.fna.gz oxyJam.ncbi.fasta.gz
+mv GCA_011076525.1_BPBGC_Naur_1.0_genomic.fna.gz netAur.ncbi.fasta.gz
+
+gunzip hetAtr.ncbi.fasta.gz
+gunzip stiNae.ncbi.fasta.gz
+gunzip oxyJam.ncbi.fasta.gz
+gunzip netAur.ncbi.fasta.gz
+
+cp -v /n/holylfs/LABS/informatics/swuitchik/ducks/ducks_cactus/*.defline.fasta .
+rm *.masked.defline.fasta
+
+for file in *.ncbi.fasta; 
+do
+	sed '/^>/s/ .*//' $file > $file.bre
+done
+
+rm *.ncbi.fasta
+
+/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/CESAR2.0/brename -p ".fasta.bre" -r ".fasta" -R
+
+# remade galloanserae.txt with new paths for gallo_ncbi.txt
+# redid cesar batch script for new files 
