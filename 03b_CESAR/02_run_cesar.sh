48ae81da0d46865138af0b1c0e45159243a124c4
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-04 12:30:59 -0400

    Update 02_run_cesar.sh

--- 03b_CESAR/02_run_cesar.sh
+++ 03b_CESAR/02_run_cesar.sh
@@ -20,20 +20,6 @@ mv cesar tools/
 # for input genes
 cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cnees/4d_sites/galGal6.gp .
 
-# for alignment
-cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/galloanserae.maf .
-#conda create -c bioconda -n cesar perl perl-scalar-util-numeric
-source activate cesar
-python3 mafSpeciesScaffoldOnly.py galloanserae.maf > galloanserae_stripped.maf
-cd ../tools
-wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedToBigBed
-chmod +x ./bedToBigBed
-chmod +x ./bedSort
-cd ..
-export cesarTools=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/CESAR2.0/tools
-export PATH=$PATH:$cesarTools
-tools/mafIndex galloanserae_stripped.maf galloanserae.bb -chromSizes=2bitdir/galGal/galGal.chrom.sizes
-
 # for 2bit dirs
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faToTwoBit
 chmod +x ./faToTwoBit
@@ -70,6 +56,20 @@ chomd +x ./brename
 ./brename -p "hetAtr.masked" -r "hetAtr" -R
 
 # sort all these files into spp-specific subdirectories in 2bitdir
+
+# for alignment
+cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/galloanserae.maf .
+#conda create -c bioconda -n cesar perl perl-scalar-util-numeric
+source activate cesar
+python3 mafSpeciesScaffoldOnly.py galloanserae.maf > galloanserae_stripped.maf
+cd ../tools
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedToBigBed
+chmod +x ./bedToBigBed
+chmod +x ./bedSort
+cd ..
+export cesarTools=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/CESAR2.0/tools
+export PATH=$PATH:$cesarTools
+tools/mafIndex galloanserae_stripped.maf galloanserae.bb -chromSizes=2bitdir/galGal/galGal.chrom.sizes
 
 #### 
 # these are weird rearrangements I had to do to get the jobList to run properly - try without these (ie/ go straight to formatGenePred.pl) and see if it works first. If not, may need these rearrangements
