da97f777490227a5f5699e53f22bcdcc4243b095
sjswuitchik <sjswuit@g.harvard.edu>
2020-05-06 11:38:28 -0400

    Added setupPhyloAcc.sh

---
+++ 03a_cnee_analysis/06_PhyloAcc/setupPhyloAcc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+module purge
+module load gcc/7.1.0-fasrc01 armadillo/7.800.2-fasrc02 gsl/2.4-fasrc01
+export PATH=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cness/PhyloAcc/:${PATH}
+
