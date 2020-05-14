60475ee217c750b0873d1fdd508f8d93df5ea0ee
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-14 14:52:44 -0400

    Update 02_wga.sh

--- 02_wga/02_wga.sh
+++ 02_wga/02_wga.sh
@@ -20,26 +20,9 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/074/415/GCA_011074415.1_BP
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/003/435/085/GCA_003435085.1_NTU_Smik_1.2/GCA_003435085.1_NTU_Smik_1.2_genomic.fna.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/870/855/GCA_001870855.1_T_cupido_pinnatus_GPC_3440_v1/GCA_001870855.1_T_cupido_pinnatus_GPC_3440_v1_genomic.fna.gz
 
-# decompress 
-
-gunzip GCF_003850225.1_IASCAAS_PekingDuck_PBH1.5_genomic.fna.gz
-gunzip GCA_002592135.1_ASM259213v1_genomic.fna.gz
-gunzip GCF_000971095.1_AnsCyg_PRJNA183603_v1.0_genomic.fna.gz
-gunzip GCA_006229135.1_ASM622913v1_genomic.fna.gz
-gunzip GCA_006130075.1_GSC_cangoose_1.0_genomic.fna.gz
-gunzip GCA_008692595.1_Cv_LA_1.0_genomic.fna.gz
-gunzip GCF_001577835.2_Coturnix_japonica_2.1_genomic.fna.gz
-gunzip GCF_000002315.6_GRCg6a_genomic.fna.gz 
-gunzip GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
-gunzip GCA_011076525.1_BPBGC_Naur_1.0_genomic.fna.gz
-gunzip GCF_002078875.1_NumMel1.0_genomic.fna.gz
-gunzip GCA_011077185.1_BPBGC_Ojam_1.0_genomic.fna.gz
-gunzip GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna.gz
-gunzip GCA_003435085.1_NTU_Smik_1.2_genomic.fna.gz
-gunzip GCA_001870855.1_T_cupido_pinnatus_GPC_3440_v1_genomic.fna.gz
+gunzip *.gz
 
 # rename 
-
 mv GCF_003850225.1_IASCAAS_PekingDuck_PBH1.5_genomic.fna anaPla.ncbi.fasta
 mv GCA_002592135.1_ASM259213v1_genomic.fna ansBra.ncbi.fasta
 mv GCF_000971095.1_AnsCyg_PRJNA183603_v1.0_genomic.fna ansCyg.ncbi.fasta
@@ -57,7 +40,6 @@ mv GCA_003435085.1_NTU_Smik_1.2_genomic.fna syrMik.ncbi.fasta
 mv GCA_001870855.1_T_cupido_pinnatus_GPC_3440_v1_genomic.fna tymCupPin.ncbi.fasta
 
 # need to modify all deflines before running CACTUS 
-
 for file in *.ncbi.fasta; 
 do
 	sed '/^>/s/ .*//' $file > $file.bre
@@ -68,5 +50,4 @@ rm *.ncbi.fasta
 /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/CESAR2.0/brename -p ".fasta.bre" -r ".fasta" -R
 
 # Run Cactus
-
 sbatch run_cactus.sh 
