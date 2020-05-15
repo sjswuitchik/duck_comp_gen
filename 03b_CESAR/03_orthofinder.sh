8e7cf363c67f58bdc5358ebf0154e45ec8208290
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-15 13:00:35 -0400

    Update 03_orthofinder.sh

--- 03b_CESAR/03_orthofinder.sh
+++ 03b_CESAR/03_orthofinder.sh
@@ -9,26 +9,23 @@ module load Anaconda3/2019.10
 #conda create -c bioconda -n ortho orthofinder gffread
 conda activate ortho
 
-
 # copy GTFs from CESAR 
 mkdir -p from_cesar/input_data
-cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/output_gtfs/cleaned_reordered_* input_data/
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/output_gtfs/cleaned_reordered_* from_cesar/
 cd from_cesar/
 # translate to protein seq
-# NB: this step creates an an index file for the reference genome in the 2bitdir - if you don't want that to happen, copy the fastas, then run in the input_data dir
-gffread cleaned_reordered_hetAtr.sorted.gtf -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/hetAtr/hetAtr.fasta -y hetAtr.translated.fa -S 
-gffread cleaned_reordered_netAur.sorted.gtf -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/netAur/netAur.fasta -y netAur.translated.fa -S
-gffread cleaned_reordered_oxyJam.sorted.gtf -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/oxyJam/oxyJam.fasta -y oxyJam.translated.fa -S
-gffread cleaned_reordered_stiNae.sorted.gtf -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/stiNae/stiNae.fasta -y stiNae.translated.fa -S
-# tidy
-mv *.translated.fa input_data/
+# NB: this step creates an an index file for the reference genome in the 2bitdir - if you don't want that, copy the fastas over, then run in your working dir
+for file in hetAtr netAur oxyJam stiNae;
+do
+  gffread cleaned_reordered_$file.sorted.gtf -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/$file/$file.fasta -y $file.translated.fa -S 
+  mv $file.translated.fa input_data/
+done
 
-# get the rest of the species GFFs from the WGA 
+# get the rest of the species annotations from the WGA 
 cd ../
 mkdir -p from_ncbi/input_data
 cd from_ncbi
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/850/225/GCF_003850225.1_IASCAAS_PekingDuck_PBH1.5/GCF_003850225.1_IASCAAS_PekingDuck_PBH1.5_genomic.gff.gz
-
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/971/095/GCF_000971095.1_AnsCyg_PRJNA183603_v1.0/GCF_000971095.1_AnsCyg_PRJNA183603_v1.0_genomic.gff.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/577/835/GCF_001577835.2_Coturnix_japonica_2.1/GCF_001577835.2_Coturnix_japonica_2.1_genomic.gff.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.6_GRCg6a/GCF_000002315.6_GRCg6a_genomic.gff.gz
@@ -42,13 +39,17 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/870/855/GCA_001870855.1_T_
 
 gunzip *.gz
 
+mv GCA_002592135.1_ASM259213v1_genomic.gbff ansBra.gb
+mv GCA_006229135.1_ASM622913v1_genomic.gbff ansInd.gb
+mv GCA_006130075.1_GSC_cangoose_1.0_genomic.gbff braCan.gb
+mv GCA_008692595.1_Cv_LA_1.0_genomic.gbff colVir.gb
+mv GCA_003435085.1_NTU_Smik_1.2_genomic.gbff syrMik.gb
+mv GCA_001870855.1_T_cupido_pinnatus_GPC_3440_v1_genomic.gbff tymCupPin.gb
+
 # convert the Genbank formats to GFF
-for file in *.gbff;
+for file in ansBra ansInd braCan colVir syrMik tymCupPin;
 do
-  mv $file $file.gb
-  /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/brename -p ".gbff.gb" -r ".gb"
-  singularity exec /n/singularity_images/informatics/maker/maker\:2.31.10--pl526_16.sif bp_genbank2gff.pl --stdout --file $file --source genbank > $file.gff
-  /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/brename -p ".gb.gff" -r ".gff"
+  singularity exec /n/singularity_images/informatics/maker/maker\:2.31.10--pl526_16.sif bp_genbank2gff.pl --stdout --file $file.gb --source genbank > $file.gff
 done
 
 # rename the NCBI GFFs 
@@ -56,14 +57,15 @@ mv GCF_000971095.1_AnsCyg_PRJNA183603_v1.0_genomic.gff ansCyg.gff
 mv GCF_001577835.2_Coturnix_japonica_2.1_genomic.gff cotJap.gff
 mv GCF_002078875.1_NumMel1.0_genomic.gff numMel.gff
 mv GCF_003850225.1_IASCAAS_PekingDuck_PBH1.5_genomic.gff anaPla.gff
+mv GCF_000002315.6_GRCg6a_genomic.gff galGal.gff
 
 # translate to protein seq
-for file in anaPla ansBra ansCyg ansInd braCan colVir cotJap galGal numMel syrMik tymCupPin;
+for file in anaPla ansBra ansCyg ansInd braCan colVir cotJap numMel syrMik tymCupPin;
 do
   gffread $file.gff -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/$file/$file.fasta -y $file.translated.fa -S
 done
 
-# had indexing issues with galGal, need to strip version information from GFF
+# need to strip version information from galGal GFF
 python3 stripGFF.py galGal.gff > galGal.stripped.gff
 gffread galGal.stripped.gff -g /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/galGal/galGal.fasta -y galGal.translated.fa -S
 
