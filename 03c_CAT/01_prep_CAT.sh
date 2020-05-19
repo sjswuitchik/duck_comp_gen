003b3274d4c5358a1a4cc0d618a8f4499198bcb2
sjswuitchik <sjswuit@g.harvard.edu>
2020-05-19 12:12:46 -0400

    Starting annotation using CAT

---
+++ 03c_CAT/01_prep_CAT.sh
@@ -0,0 +1,3 @@
+singularity shell --cleanenv /n/singularity_images/informatics/cat/cat:20200116.sif
+hal2maf galloanserae.hal galloForCAT.maf --refGenome galGal --noAncestors --noDupes --targetGenomes galGal,hetAtr,netAur,oxyJam,stiNae
+
