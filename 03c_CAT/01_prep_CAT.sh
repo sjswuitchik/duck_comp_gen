0692e7c564bb1136111d53a0f5e453e6de467a7a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-19 12:15:18 -0400

    Update 01_prep_CAT.sh

--- 03c_CAT/01_prep_CAT.sh
+++ 03c_CAT/01_prep_CAT.sh
@@ -1,3 +1,11 @@
+##############################
+## Gene annotation with CAT ##
+##############################
+
+# in /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/
+
 singularity shell --cleanenv /n/singularity_images/informatics/cat/cat:20200116.sif
+
+# extract only focal and reference species from WGA
 hal2maf galloanserae.hal galloForCAT.maf --refGenome galGal --noAncestors --noDupes --targetGenomes galGal,hetAtr,netAur,oxyJam,stiNae
 
