1a1540c3879ce9e95542daf8fe64b0a0213d897f
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-20 09:31:53 -0400

    Create phyloP_test.sh

---
+++ 03a_cnee_analysis/phyloP_test.sh
@@ -0,0 +1,12 @@
+#################
+## PhyloP test ##
+#################
+
+module load Anaconda3/2019.10
+
+# in /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cnees/phyloP_test
+
+conda activate phast 
+cp -v ../PhyloAcc/input_data/galloseq_gapFixed.fa ../PhyloAcc/input_data/galloseq.part.bed ../PhyloAcc/input_data/galloTop1.named.mod .
+# usage: phyloP [OPTIONS] tree.mod [alignment] > out
+phyloP --method LRT --mode CON --msa-format FASTA --features galloseq.part.bed galloTop1.named.mod galloseq_gapFixed.fa > cnees_phyloP.out
