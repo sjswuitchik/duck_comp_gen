3965b84045933c0931a8425d3dd657eed4a11273
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-28 12:24:13 -0400

    Update 01_prep_CAT.sh

--- 03c_CAT/01_prep_CAT.sh
+++ 03c_CAT/01_prep_CAT.sh
@@ -9,12 +9,16 @@ git clone https://github.com/ComparativeGenomicsToolkit/Comparative-Annotation-T
 pip install -e Comparative-Annotation-Toolkit
 
 # extract only focal and reference species from WGA
+mkdir -p Comparative-Annotation-Toolkit/input_data
 singularity shell --cleanenv /n/singularity_images/informatics/cat/cat:20200116.sif
-hal2maf ../galloanserae.hal galloForCAT.maf --refGenome galGal --noAncestors --noDupes --targetGenomes galGal,hetAtr,netAur,oxyJam,stiNae
+hal2maf galloanserae.hal galloForCAT.maf --refGenome galGal --noAncestors --noDupes --targetGenomes galGal,hetAtr,netAur,oxyJam,stiNae
 maf2hal galloForCAT.maf galloForCAT.hal --refGenome galGal
 exit 
 
-
+# get input data set up
+mv galloForCAT.hal Comparative-Annotation-Toolkit/input_data
+cd Comparative-Annotation-Toolkit/input_data/
+cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cnees/4d_sites/galGal6.filtpy.gff . 
 
 
 
