eef6070d62e4d1bcb75dcc2ab526ac72f92278c4
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-05-28 14:33:16 -0400

    Update 01_prep_CAT.sh

--- 03c_CAT/01_prep_CAT.sh
+++ 03c_CAT/01_prep_CAT.sh
@@ -4,6 +4,8 @@
 
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/
 
+module load samtools/1.10-fasrc01
+
 # set up CAT
 git clone https://github.com/ComparativeGenomicsToolkit/Comparative-Annotation-Toolkit.git
 pip install -e Comparative-Annotation-Toolkit
@@ -15,10 +17,18 @@ hal2maf galloanserae.hal galloForCAT.maf --refGenome galGal --noAncestors --noDu
 maf2hal galloForCAT.maf galloForCAT.hal --refGenome galGal
 exit 
 
-# get input data set up
+# set up input data
 mv galloForCAT.hal Comparative-Annotation-Toolkit/input_data
 cd Comparative-Annotation-Toolkit/input_data/
+# get filtered GFF for galGal
 cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cnees/4d_sites/galGal6.filtpy.gff . 
 
+# get galGal protein alignment
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.6_GRCg6a/GCF_000002315.6_GRCg6a_protein.faa.gz
 
+# get indexed fastas for focal and reference species
+for SP in galGal hetAtr netAur oxyJam stiNae;
+do
+  cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/$SP/$SP.fasta /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/cesar/2bitdir/$SP/$SP.fasta.fai .
+done
 
