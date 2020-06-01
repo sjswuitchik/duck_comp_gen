ff22c904401b16bd32ff9d58179d9dac321d4391
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-01 10:21:00 -0400

    Update 01_orthofinder.sh

--- 04_OrthoFinder/01_orthofinder.sh
+++ 04_OrthoFinder/01_orthofinder.sh
@@ -7,7 +7,7 @@ module load Anaconda3/2019.10
 # from /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/orthos
 # NB: input data is all generated in 03b_CESAR/03_BUSCO.sh
 
-#conda create -c bioconda -n ortho orthofinder
+#conda create -c bioconda -n ortho orthofinder diamond
 conda activate ortho
 
 # run OrthoFinder 
