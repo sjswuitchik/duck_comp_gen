cdd499403eeec184a4bd074ac128b855deae26f9
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-02 11:26:37 -0400

    Update 01_orthofinder.sh

--- 04_OrthoFinder/01_orthofinder.sh
+++ 04_OrthoFinder/01_orthofinder.sh
@@ -10,7 +10,15 @@ module load Anaconda3/2019.10
 #conda create -c bioconda -n ortho orthofinder diamond
 conda activate ortho
 
+# fix annoying scaffold version issues 
+cd from_cesar/input_data/
+for SP in hetAtr netAur oxyJam stiNae;
+do
+	sed '/^>/s/\./_/g' $SP.translated.fa > $SP.translated.clean.fa
+done
+cd ../..
+
 # run OrthoFinder 
 mkdir -p run_ortho/input_data
-cp -v from_cesar/input_data/* from_ncbi/input_data/* run_ortho/input_data
+cp -v from_cesar/input_data/*.clean.fa from_ncbi/input_data/* run_ortho/input_data
 orthofinder -o run_ortho/ -f run_ortho/input_data
