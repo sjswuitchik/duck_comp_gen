b0a4c1ad09e58a111c7029e36a5fc2b7f22befbd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-12 10:49:43 -0400

    Update 01_prep_CAT.sh

--- 03c_CAT/01_prep_CAT.sh
+++ 03c_CAT/01_prep_CAT.sh
@@ -36,20 +36,4 @@ done
 
 cd ..
 
-# run CAT
-singularity exec --cleanenv --home=/var/empty /n/singularity_images/informatics/cat/cat:20200604.sif \
-luigi \
-  --module cat RunCat \
-  --hal=input_data/galloForCAT.hal \
-  --ref-genome=galGal \
-  --workers=24 \
-  --config=input_data/cat.config \
-  --target-genomes='("hetAtr", "netAur", "oxyJam", "stiNae")' \
-  --local-scheduler \
-  --binary-mode local \
-  --augustus \
-  --augustus-species=chicken \
-  --augustus-cgp \
-  --maf-chunksize 850000 \
-  --maf-overlap 250000 \
-  --assembly-hub > stdout.log 2> stdout.err
+
