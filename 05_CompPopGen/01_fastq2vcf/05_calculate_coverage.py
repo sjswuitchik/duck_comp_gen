c2d216132cd3e01d15c6050e751fb55c2b90861a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-29 15:55:34 -0400

    Update 05_calculate_coverage.py

--- 05_CompPopGen/01_fastq2vcf/05_calculate_coverage.py
+++ 05_CompPopGen/01_fastq2vcf/05_calculate_coverage.py
@@ -337,7 +337,7 @@ def sum_coverage_sbatch(sp_dir,sp_abbr):
     cmd_1 = 'module load bedtools2/2.26.0-fasrc01\nmodule load Anaconda/5.0.1-fasrc01\nsource activate pipeline'
 
     #run python script to sum, create histogram if missing, and create clean sites, too high sites, and too low sites bedfiles
-    cmd_2 = 'python ../comp-pop-gen/fastq_to_vcf/pipeline_scripts/05_01_sum_coverage_subscript.py --sp_dir %s --sp_abbr %s'%(sp_dir,sp_abbr)
+    cmd_2 = 'python 05_01_sum_coverage_subscript.py --sp_dir %s --sp_abbr %s'%(sp_dir,sp_abbr)
 
     #sort and merge all bedgraphs
     cmd_3 = 'sort -k 1,1 -k2,2n %s/stats_coverage/_%s_clean_coverage_sites.bed | bedtools merge -i stdin > %s/stats_coverage/_%s_clean_coverage_sites_merged.bed'%(sp_dir,sp_abbr,sp_dir,sp_abbr)
