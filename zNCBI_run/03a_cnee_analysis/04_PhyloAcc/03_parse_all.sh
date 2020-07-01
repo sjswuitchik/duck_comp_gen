eb696d52eac8aea86cd16155607ebf5b74431ed7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-01 10:20:17 -0400

    Update 03_parse_all.sh

--- zNCBI_run/03a_cnee_analysis/04_PhyloAcc/03_parse_all.sh
+++ zNCBI_run/03a_cnee_analysis/04_PhyloAcc/03_parse_all.sh
@@ -2,19 +2,18 @@
 
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc
 
-VERSION=$1
 cd top1_outs
 ../concat_output.sh elem_lik
-mv elem_lik_combined.txt ../elem_lik_combined_top1_${VERSION}.txt
+mv elem_lik_combined.txt ../elem_lik_combined_top1.txt
 ../concat_output.sh  rate_postZ_M2
-mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top1_${VERSION}.txt
+mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top1.txt
 cd ../top2_outs
 ../concat_output.sh elem_lik
-mv elem_lik_combined.txt ../elem_lik_combined_top2_${VERSION}.txt
+mv elem_lik_combined.txt ../elem_lik_combined_top2.txt
 ../concat_output.sh  rate_postZ_M2
-mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top2_${VERSION}.txt
+mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top2.txt
 cd ../top3_outs
 ../concat_output.sh elem_lik
-mv elem_lik_combined.txt ../elem_lik_combined_top3_${VERSION}.txt
+mv elem_lik_combined.txt ../elem_lik_combined_top3.txt
 ../concat_output.sh  rate_postZ_M2
-mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top3_${VERSION}.txt
+mv rate_postZ_M2_combined.txt ../rate_postZ_M2_combined_top3.txt
