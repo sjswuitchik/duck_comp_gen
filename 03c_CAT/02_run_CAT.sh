a62fd2f6f453cc4d608bcca1f85e47554497a713
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-01 13:09:50 -0400

    Update 02_run_CAT.sh

--- 03c_CAT/02_run_CAT.sh
+++ 03c_CAT/02_run_CAT.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH -p shared
+#SBATCH -p holy-info
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --mem 4000
-#SBATCH -t 02:00:00
+#SBATCH --mem=0
+#SBATCH --exclusive
+#SBATCH -t 6-00:00:00
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
 singularity exec --cleanenv --home=/var/empty /n/singularity_images/informatics/cat/cat:20200604.sif \
 luigi \
   --module cat RunCat \
-  --hal=input_data/galloForCAT.hal \
+  --hal=testAll_input/galloanserae.hal \
   --ref-genome=galGal \
   --workers=24 \
-  --config=input_data/cat.config \
-  --target-genomes='("hetAtr", "netAur", "oxyJam", "stiNae")' \
+  --config=testAll_input/testAll_cat.config \
+  --target-genomes='("braCan", "ansInd", "ansCyg", "ansBra", "netAur", "oxyJam", "hetAtr", "stiNae", "numMel", "colVir", "tymCupPin", "syrMik", "cotJap")' \
   --local-scheduler \
   --binary-mode local \
   --augustus \
@@ -22,4 +22,4 @@ luigi \
   --augustus-cgp \
   --maf-chunksize 850000 \
   --maf-overlap 250000 \
-  --assembly-hub > stdout.log 2> stdout.err
+  --assembly-hub
