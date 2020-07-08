957c2ccc66554dc460401c8bfe227cd9052505d7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-08 15:24:07 -0400

    Update hetAtr_longranger.sh

--- 05_CompPopGen/01_fastq2vcf/hetAtr_longranger.sh
+++ 05_CompPopGen/01_fastq2vcf/hetAtr_longranger.sh
@@ -3,4 +3,9 @@
 
 module load longranger/2.2.2-fasrc01
 
-longranger mkfastq 
+longranger wgs --id hetAtrS1 \
+--fastqs=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/longranger_proc/raw_fastqs/04_hetAtr \
+--sex=female
+
+
+--run=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/longranger_proc/raw_fastqs/04_hetAtr --sample=DGAB-CNB0004-CN4-lib1
