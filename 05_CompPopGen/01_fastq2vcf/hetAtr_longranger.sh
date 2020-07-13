5bdb6df629c7c0bdbbfb3904f9259a1dd3bc12ea
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-13 12:44:13 -0400

    Update hetAtr_longranger.sh

--- 05_CompPopGen/01_fastq2vcf/hetAtr_longranger.sh
+++ 05_CompPopGen/01_fastq2vcf/hetAtr_longranger.sh
@@ -10,14 +10,15 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/011/077/185/GCF_011077185.1_BP
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/074/415/GCA_011074415.1_BPBGC_Snae_1.0/GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna.gz
 gunzip *.gz
 
-longranger mkref 
-
-
+longranger mkref GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna
+longranger mkref GCA_011076525.1_BPBGC_Naur_1.0_genomic.fna
+longranger mkref GCF_011077185.1_BPBGC_Ojam_1.0_genomic.fna
+longranger mkref GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna
 
 # run longranger
 longranger wgs --id hetAtr \
 --fastqs=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/longranger_proc/raw_fastqs/04_hetAtr \
 --sex=female \
---reference=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/longranger_proc/refdata-b37-2.1.0 \
+--reference=/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/longranger_proc/refdata-GCA_011075105.1_BPBGC_Hatr_1.0_genomic \
 --vcmode=gatk:/n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/gatk-4.0.3.0/gatk-package-4.0.3.0-local.jar
 
