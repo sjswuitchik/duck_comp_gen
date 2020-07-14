44d607f979cf3a697a2cd61538a187297681bdb1
sjswuitchik <sjswuit@g.harvard.edu>
2020-07-14 10:31:07 -0400

    Update new image for CAT

--- 03c_CAT/02_run_CAT.sh
+++ 03c_CAT/02_run_CAT.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 #SBATCH -p holy-info
-#SBATCH -N 1
+#SBATCH --nodes=1
 #SBATCH --mem=0
 #SBATCH --exclusive
 #SBATCH -t 6-00:00:00
-#SBATCH -o slurm.%N.%j.out
-#SBATCH -e slurm.%N.%j.err
 
-singularity exec --cleanenv --home=/var/empty /n/singularity_images/informatics/cat/cat:20200604.sif \
+set -o nounset -o errexit -o xtrace
+
+export SINGULARITYENV_PYTHONNOUSERSITE=1
+
+readonly SINGULARITY_IMAGE=/n/singularity_images/informatics/cat/cat_v2.1.0-cf7a06e.sif
+if [ ! -s cat_work.img ]
+then
+  readonly tmpdir=$(mktemp -d)
+  mkdir -m 777 -p ${tmpdir}/upper
+  truncate -s 1T cat_work.img
+  singularity exec --cleanenv ${SINGULARITY_IMAGE} mkfs.ext3 -d "${tmpdir}" cat_work.img
+  singularity exec --cleanenv --overlay cat_work.img ${SINGULARITY_IMAGE} mkdir /cat_work
+  ln -sf /cat_work cat_work
+  rm -rf "${tmpdir}"
+fi
+srun -n 1 env time -v singularity exec \
+                           --cleanenv \
+                           --no-home \
+                           --overlay cat_work.img \
+                           "${SINGULARITY_IMAGE}" \
 luigi \
   --module cat RunCat \
   --hal=testAll_input/galloanserae.hal \
   --ref-genome=galGal \
-  --workers=24 \
+  --workers=$((SLURM_JOB_CPUS_PER_NODE-1)) \
   --config=testAll_input/testAll_cat.config \
-  --target-genomes='("braCan", "ansInd", "ansCyg", "ansBra", "netAur", "oxyJam", "hetAtr", "stiNae", "numMel", "colVir", "tymCupPin", "syrMik", "cotJap")' \
+  --target-genomes='("netAur", "oxyJam", "hetAtr", "stiNae")' \
   --local-scheduler \
   --binary-mode local \
   --augustus \
