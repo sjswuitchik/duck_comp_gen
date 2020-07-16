8cc768bf568b6e6d497c51815f5acc7126421a18
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-16 17:25:58 -0400

    Update 01_orthofinder.sh

--- 04_OrthoFinder/01_orthofinder.sh
+++ 04_OrthoFinder/01_orthofinder.sh
@@ -7,19 +7,23 @@ module load Anaconda3/2019.10
 # from /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/orthos
 # NB: input data is all generated in 03b_CESAR/03_BUSCO.sh
 
-#conda create -c bioconda -n ortho orthofinder diamond
+#conda create -c bioconda -n ortho orthofinder diamond 
+#conda install -c conda-forge perl
+conda activate ortho
 
-# issues with duplicate sequences in hetAtr; janky grep to remove
+# issues with duplicate sequences in ducks, need to remove 
 cd from_cesar/input_data/
-grep -v -f dup1.txt hetAtr.translated.fa | grep -v -f dup2.txt > hetAtr.translated.dedup.fa
-
-# fix annoying scaffold version issues 
-for SP in netAur oxyJam stiNae;
+for SP in hetAtr netAur oxyJam stiNae;
 do
-	sed '/^>/s/\./_/g' $SP.translated.fa > $SP.translated.clean.fa
+	perl /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/orthos/rm_dup_fa.pl $SP.translated.fa
 done
 
-sed '/^>/s/\./_/g' hetAtr.translated.dedup.fa > hetAtr.translated.clean.fa
+# fix annoying scaffold version issues 
+for SP in hetAtr netAur oxyJam stiNae;
+do
+	sed '/^>/s/\./_/g' $SP.translated_uniq.fa > $SP.translated.clean.fa
+done
+
 cd ../..
 
 # run OrthoFinder 
