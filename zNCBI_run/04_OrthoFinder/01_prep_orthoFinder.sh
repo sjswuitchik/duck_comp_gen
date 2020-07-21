10005dfbdd0bb10ae7be6e4d271d11e846793ace
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-21 13:37:33 -0400

    Update 01_prep_orthoFinder.sh

--- zNCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
+++ zNCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
@@ -14,6 +14,8 @@ mkdir -p run_ortho/input_data
 cd run_ortho/input_data
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/011/077/185/GCF_011077185.1_BPBGC_Ojam_1.0/GCF_011077185.1_BPBGC_Ojam_1.0_protein.faa.gz
 gunzip *.gz
+cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/orthos/run_ortho/input_data/galGal.translated.fa .
+cd ../..
 
 # run OrthoFinder 
 sbatch 02_ncbi_ortho.sh
