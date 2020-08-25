b73d275550f346c8be178b7b8d97098c516a75d0
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-08-25 15:45:34 -0400

    Update phyloP_test.sh

--- 03a_cnee_analysis/phyloP_test.sh
+++ 03a_cnee_analysis/phyloP_test.sh
@@ -8,10 +8,8 @@ module load Anaconda3/2019.10
 
 conda activate phast 
 cp -v ../PhyloAcc/input_data/galloseq_gapFixed.fa ../PhyloAcc/input_data/galloseq.part.bed ../PhyloAcc/input_data/galloTop*.named.mod .
+
 # usage: phyloP [OPTIONS] tree.mod [alignment] > out
 phyloP --method LRT --mode CON --msa-format FASTA --features galloseq.part.bed galloTop1.named.mod galloseq_gapFixed.fa > cnees_phyloP.out
-
 phyloP --method LRT --mode CON --msa-format FASTA --features galloseq.part.bed galloTop2.named.mod galloseq_gapFixed.fa > cnees_phyloP_top2.out
-
 phyloP --method LRT --mode CON --msa-format FASTA --features galloseq.part.bed galloTop3.named.mod galloseq_gapFixed.fa > cnees_phyloP_top3.out
-
