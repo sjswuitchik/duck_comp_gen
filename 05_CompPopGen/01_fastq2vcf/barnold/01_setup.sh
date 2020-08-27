71b5a0897a3ceb2f084ec9e16d21c3cd4033e330
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-08-27 15:33:32 -0400

    Update 01_setup.sh

--- 05_CompPopGen/01_fastq2vcf/barnold/01_setup.sh
+++ 05_CompPopGen/01_fastq2vcf/barnold/01_setup.sh
@@ -5,12 +5,19 @@ git clone https://github.com/harvardinformatics/shortRead_mapping_variantCalling
 cd shortRead_mapping_variantCalling
 
 # make directories for data
-mkdir data/stiNae
-mkdir data/hetAtr
+mkdir -p data/stiNae/fastqs
+mkdir -p data/hetAtr/fastqs
+mkdir -p data/hetAtr/genome
 
 # copy fastqs over 
-cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/stiNae_male/*.gz data/stiNae
-cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/hetAtr_reseq/*.gz data/hetAtr
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/stiNae_male/*.gz data/stiNae/fastqs
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/hetAtr_reseq/*.gz data/hetAtr/fastqs
+
+# get reference genome
+cd data/hetAtr/genome
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/011/075/105/GCA_011075105.1_BPBGC_Hatr_1.0/GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
+gunzip GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna.gz
+mv GCA_011075105.1_BPBGC_Hatr_1.0_genomic.fna hetAtr.fa
 
 # concat fastqs (in same order!) for individuals 
 ./concat_fastqs.sh 
