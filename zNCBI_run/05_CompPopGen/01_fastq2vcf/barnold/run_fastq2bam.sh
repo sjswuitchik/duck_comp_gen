c0938c01bb676261b7d7c1c48c43dd13904b837c
sjswuitchik <sjswuit@g.harvard.edu>
2020-08-27 16:17:47 -0400

    Reorg with new fastq2vcf pipeline

---
+++ zNCBI_run/05_CompPopGen/01_fastq2vcf/barnold/run_fastq2bam.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J sm
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p holy-info
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=10000
+
+conda activate snakemake
+snakemake --snakefile Snakefile_fastq2bam --profile ./profiles/slurm
