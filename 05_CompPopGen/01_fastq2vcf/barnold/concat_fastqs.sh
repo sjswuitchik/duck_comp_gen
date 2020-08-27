6e5e6682cd5a4d870ff17fa494cd7ac9be946645
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-08-27 15:14:15 -0400

    Create concat_fastqs.sh

---
+++ 05_CompPopGen/01_fastq2vcf/barnold/concat_fastqs.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/bash
+
+# ind01
+cat hetAtr_ind01_L1_573_1.fastq.gz hetAtr_ind01_L1_574_1.fastq.gz hetAtr_ind01_L1_575_1.fastq.gz hetAtr_ind01_L1_576_1.fastq.gz hetAtr_ind01_L1_577_1.fastq.gz hetAtr_ind01_L1_578_1.fastq.gz hetAtr_ind01_L1_579_1.fastq.gz hetAtr_ind01_L1_580_1.fastq.gz > hetAtr_ind01_1.fastq.gz
+
+cat hetAtr_ind01_L1_573_2.fastq.gz hetAtr_ind01_L1_574_2.fastq.gz hetAtr_ind01_L1_575_2.fastq.gz hetAtr_ind01_L1_576_2.fastq.gz hetAtr_ind01_L1_577_2.fastq.gz hetAtr_ind01_L1_578_2.fastq.gz hetAtr_ind01_L1_579_2.fastq.gz hetAtr_ind01_L1_580_2.fastq.gz > hetAtr_ind01_2.fastq.gz
+
+# ind02
+cat hetAtr_ind02_L1_581_1.fastq.gz hetAtr_ind02_L1_582_1.fastq.gz hetAtr_ind02_L1_583_1.fastq.gz hetAtr_ind02_L1_584_1.fastq.gz > hetAtr_ind02_1.fastq.gz
+
+cat hetAtr_ind02_L1_581_2.fastq.gz hetAtr_ind02_L1_582_2.fastq.gz hetAtr_ind02_L1_583_2.fastq.gz hetAtr_ind02_L1_584_2.fastq.gz > hetAtr_ind02_2.fastq.gz
+
+# ind03
+cat hetAtr_ind03_L1_533_1.fastq.gz hetAtr_ind03_L1_534_1.fastq.gz hetAtr_ind03_L1_535_1.fastq.gz hetAtr_ind03_L1_536_1.fastq.gz hetAtr_ind03_L2_533_1.fastq.gz hetAtr_ind03_L2_534_1.fastq.gz hetAtr_ind03_L2_535_1.fastq.gz hetAtr_ind03_L2_536_1.fastq.gz > hetAtr_ind03_1.fastq.gz
+
+cat hetAtr_ind03_L1_533_2.fastq.gz hetAtr_ind03_L1_534_2.fastq.gz hetAtr_ind03_L1_535_2.fastq.gz hetAtr_ind03_L1_536_2.fastq.gz hetAtr_ind03_L2_533_2.fastq.gz hetAtr_ind03_L2_534_2.fastq.gz hetAtr_ind03_L2_535_2.fastq.gz hetAtr_ind03_L2_536_2.fastq.gz > hetAtr_ind03_2.fastq.gz
+
+# ind04
+cat hetAtr_ind04_L3_505_1.fastq.gz hetAtr_ind04_L3_506_1.fastq.gz hetAtr_ind04_L3_507_1.fastq.gz hetAtr_ind04_L3_508_1.fastq.gz > hetAtr_ind04_1.fastq.gz
+
+cat hetAtr_ind04_L3_505_2.fastq.gz hetAtr_ind04_L3_506_2.fastq.gz hetAtr_ind04_L3_507_2.fastq.gz hetAtr_ind04_L3_508_2.fastq.gz > hetAtr_ind04_2.fastq.gz
+ 
+# ind05
+cat hetAtr_ind05_L1_521_1.fastq.gz hetAtr_ind05_L1_522_1.fastq.gz hetAtr_ind05_L1_523_1.fastq.gz hetAtr_ind05_L1_524_1.fastq.gz > hetAtr_ind05_1.fastq.gz
+
+cat hetAtr_ind05_L1_521_2.fastq.gz hetAtr_ind05_L1_522_2.fastq.gz hetAtr_ind05_L1_523_2.fastq.gz hetAtr_ind05_L1_524_2.fastq.gz > hetAtr_ind05_2.fastq.gz
+
+# ind06
+cat hetAtr_ind06_L1_513_1.fastq.gz hetAtr_ind06_L1_514_1.fastq.gz hetAtr_ind06_L1_515_1.fastq.gz hetAtr_ind06_L1_516_1.fastq.gz > hetAtr_ind06_1.fastq.gz
+
+cat hetAtr_ind06_L1_513_2.fastq.gz hetAtr_ind06_L1_514_2.fastq.gz hetAtr_ind06_L1_515_2.fastq.gz hetAtr_ind06_L1_516_2.fastq.gz > hetAtr_ind06_2.fastq.gz
+
+# ind07
+cat hetAtr_ind07_L3_513_1.fastq.gz hetAtr_ind07_L3_514_1.fastq.gz hetAtr_ind07_L3_515_1.fastq.gz hetAtr_ind07_L3_516_1.fastq.gz > hetAtr_ind07_1.fastq.gz
+
+cat hetAtr_ind07_L3_513_2.fastq.gz hetAtr_ind07_L3_514_2.fastq.gz hetAtr_ind07_L3_515_2.fastq.gz hetAtr_ind07_L3_516_2.fastq.gz > hetAtr_ind07_2.fastq.gz
+
+# ind08
+cat hetAtr_ind08_L1_501_1.fastq.gz hetAtr_ind08_L1_502_1.fastq.gz hetAtr_ind08_L1_503_1.fastq.gz hetAtr_ind08_L1_504_1.fastq.gz > hetAtr_ind08_1.fastq.gz
+
+cat hetAtr_ind08_L1_501_2.fastq.gz hetAtr_ind08_L1_502_2.fastq.gz hetAtr_ind08_L1_503_2.fastq.gz hetAtr_ind08_L1_504_2.fastq.gz > hetAtr_ind08_2.fastq.gz
+
+# ind09
+cat hetAtr_ind09_L1_529_1.fastq.gz hetAtr_ind09_L1_530_1.fastq.gz hetAtr_ind09_L1_531_1.fastq.gz hetAtr_ind09_L1_532_1.fastq.gz > hetAtr_ind09_1.fastq.gz
+
+cat hetAtr_ind09_L1_529_2.fastq.gz hetAtr_ind09_L1_530_2.fastq.gz hetAtr_ind09_L1_531_2.fastq.gz hetAtr_ind09_L1_532_2.fastq.gz > hetAtr_ind09_2.fastq.gz
+
+# ind10
+cat hetAtr_ind10_L1_505_1.fastq.gz hetAtr_ind10_L1_506_1.fastq.gz hetAtr_ind10_L1_507_1.fastq.gz hetAtr_ind10_L1_508_1.fastq.gz > hetAtr_ind10_1.fastq.gz
+
+cat hetAtr_ind10_L1_505_2.fastq.gz hetAtr_ind10_L1_506_2.fastq.gz hetAtr_ind10_L1_507_2.fastq.gz hetAtr_ind10_L1_508_2.fastq.gz > hetAtr_ind10_2.fastq.gz
+
+# ind11
+cat hetAtr_ind11_L2_521_1.fastq.gz hetAtr_ind11_L2_522_1.fastq.gz hetAtr_ind11_L2_523_1.fastq.gz hetAtr_ind11_L2_524_1.fastq.gz > hetAtr_ind11_1.fastq.gz
+
+cat hetAtr_ind11_L2_521_2.fastq.gz hetAtr_ind11_L2_522_2.fastq.gz hetAtr_ind11_L2_523_2.fastq.gz hetAtr_ind11_L2_524_2.fastq.gz > hetAtr_ind11_2.fastq.gz
+
+# ind12
+cat hetAtr_ind12_L4_565_1.fastq.gz hetAtr_ind12_L4_566_1.fastq.gz hetAtr_ind12_L4_567_1.fastq.gz hetAtr_ind12_L4_568_1.fastq.gz hetAtr_ind12_L4_569_1.fastq.gz hetAtr_ind12_L4_570_1.fastq.gz hetAtr_ind12_L4_571_1.fastq.gz hetAtr_ind12_L4_572_1.fastq.gz > hetAtr_ind12_1.fastq.gz
+
+cat hetAtr_ind12_L4_565_2.fastq.gz hetAtr_ind12_L4_566_2.fastq.gz hetAtr_ind12_L4_567_2.fastq.gz hetAtr_ind12_L4_568_2.fastq.gz hetAtr_ind12_L4_569_2.fastq.gz hetAtr_ind12_L4_570_2.fastq.gz hetAtr_ind12_L4_571_2.fastq.gz hetAtr_ind12_L4_572_2.fastq.gz > hetAtr_ind12_2.fastq.gz
+
+# ind13
+cat hetAtr_ind13_L4_501_1.fastq.gz hetAtr_ind13_L4_502_1.fastq.gz hetAtr_ind13_L4_503_1.fastq.gz hetAtr_ind13_L4_504_1.fastq.gz > hetAtr_ind13_1.fastq.gz
+
+cat hetAtr_ind13_L4_501_2.fastq.gz hetAtr_ind13_L4_502_2.fastq.gz hetAtr_ind13_L4_503_2.fastq.gz hetAtr_ind13_L4_504_2.fastq.gz > hetAtr_ind13_2.fastq.gz
+
+# ind14
+cat hetAtr_ind14_L2_549_1.fastq.gz hetAtr_ind14_L2_550_1.fastq.gz hetAtr_ind14_L2_551_1.fastq.gz hetAtr_ind14_L2_552_1.fastq.gz > hetAtr_ind14_1.fastq.gz
+
+cat hetAtr_ind14_L2_549_2.fastq.gz hetAtr_ind14_L2_550_2.fastq.gz hetAtr_ind14_L2_551_2.fastq.gz hetAtr_ind14_L2_552_2.fastq.gz > hetAtr_ind14_2.fastq.gz
