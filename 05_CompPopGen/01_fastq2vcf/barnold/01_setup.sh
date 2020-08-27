9ac822b90f8ef94642f23cfe2c33b3798642518d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-08-27 14:48:18 -0400

    Update 01_setup.sh

--- 05_CompPopGen/01_fastq2vcf/barnold/01_setup.sh
+++ 05_CompPopGen/01_fastq2vcf/barnold/01_setup.sh
@@ -1,11 +1,14 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/
 
+# clone pipeline & repo 
 git clone https://github.com/harvardinformatics/shortRead_mapping_variantCalling
 cd shortRead_mapping_variantCalling
 
+# make directories for data
 mkdir data/stiNae
 mkdir data/hetAtr
 
+# copy fastqs over 
 cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/stiNae_male/*.gz data/stiNae
-cp -vr /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/hetAtr_reseq/*.gz data/hetAtr
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/comppopgen/orig_fastqs/hetAtr_reseq/*.gz data/hetAtr
 
