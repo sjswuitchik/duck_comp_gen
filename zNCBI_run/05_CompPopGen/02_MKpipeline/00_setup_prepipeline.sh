87406c9b251851beca4237e2b390e582d8957521
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-09-16 09:49:17 -0400

    Update 00_setup_prepipeline.sh

--- zNCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
+++ zNCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
@@ -1,19 +1,15 @@
-# in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/01_fastq2vcf/shortRead_mapping_variantCalling
+# in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/01_fastq2vcf/shortRead_mapping_variantCalling/gatk/
 
 # there is a little bit of prep that needs to be done before the output from the snakemake pipeline will be suitable to work in the MK pipeline
 
 module load bcftools/1.5-fasrc02
 
-# pull out the stiNae individual to its own VCF
-bcftools view -c1 -Oz -s stiNae_ind01 -o stiNae.vcf.gz Combined_hardFiltered.vcf
-
-mv Combined_hardFiltered.vcf hetAtr.vcf.gz
-
-# move VCFs and missingness over to MK dir
-mv stiNae.vcf.gz hetAtr.vcf.gz /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/02_MK_pipeline
-mv missing_data_per_ind.txt /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/02_MK_pipeline
-
+cp Combined_hardFiltered.vcf missing_data_per_ind.txt /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/02_MK_pipeline 
 cd /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/02_MK_pipeline
+
+# pull out the stiNae individual to its own VCF
+bcftools view -O z -s stiNae_ind01 Combined_hardFiltered.vcf > stiNae.vcf.gz
+bcftools view -O z -S hetAtr_indvs Combined_hardFiltered.vcf > hetAtr.vcf.gz
 
 # pull out stiNae from missingness file & rename
 grep -v stiNae_ind01 missing_data_per_ind.txt > hetAtr_all_all_missingness_info.txt
