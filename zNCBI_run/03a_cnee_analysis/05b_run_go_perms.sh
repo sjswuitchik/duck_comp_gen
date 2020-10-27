a403a9e579d252b184e61f85e4ee195fca3c4f8e
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-27 15:05:36 -0400

    Update 05b_run_go_perms.sh

--- zNCBI_run/03a_cnee_analysis/05b_run_go_perms.sh
+++ zNCBI_run/03a_cnee_analysis/05b_run_go_perms.sh
@@ -5,7 +5,7 @@
 #SBATCH -p serial_requeue,bos-info
 #SBATCH -n 5
 #SBATCH -N 1
-#SBATCH --array=1-100
+#SBATCH --array=1-50
 
 DATAPATH=/n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/postPhyloAcc/go_perms
 Rscript cnee_go_perms.R ${SLURM_ARRAY_TASK_ID} 5 50 $DATAPATH 
