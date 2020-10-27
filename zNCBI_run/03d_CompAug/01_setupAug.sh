6fa8eaa3c56c71199c4e9a9090198f59a25cdc33
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-26 20:32:37 -0400

    Update 01_setupAug.sh

--- zNCBI_run/03d_CompAug/01_setupAug.sh
+++ zNCBI_run/03d_CompAug/01_setupAug.sh
@@ -1,7 +1,7 @@
 ## in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03d_CompAug/
 
 module load Anaconda/5.0.1-fasrc01
-#conda create -n compAug dockermake boost gsl lpsolve55 pymysql bamtools bcftools htslib samtools
+#conda create -n compAug dockermake boost boost-cpp libboost gsl lpsolve55 pymysql bamtools bcftools htslib samtools
 source activate compAug
 
 # install Comp Aug
