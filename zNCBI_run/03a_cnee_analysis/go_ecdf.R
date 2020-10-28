86a684bee60fe1c00eabc2261e0b2e6d352633fb
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-28 10:17:53 -0400

    Update go_ecdf.R

--- zNCBI_run/03a_cnee_analysis/go_ecdf.R
+++ zNCBI_run/03a_cnee_analysis/go_ecdf.R
@@ -3,7 +3,7 @@ library(tidyverse)
 wd <- "/n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/postPhyloAcc/go_perms"
 
 for (whichont in c("BP", "MF")) {
-  spec_patt <- glob2rx(paste0("GO_top1_*_", whichont, "perm.tsv"))
+  spec_patt <- glob2rx(paste0("GO_top1_*_", whichont, "_perm.tsv"))
   files <- list.files(path = wd, pattern = spec_patt, full.names = T)
   results <- list()
   for (file in files) {
@@ -16,12 +16,11 @@ for (whichont in c("BP", "MF")) {
     summarize(ecdf_frac = list(ecdf(target_frac)),
               ecdf_enrich = list(ecdf(enrich)),
               ecdf_logp = list(ecdf(logp.perm))) %>%
-    saveRDS(file = paste0(wdir, "GO_top1_", whichont, ".robj"))
+    saveRDS(file = paste0(wd, "GO_top1_", whichont, ".robj"))
 }
 
-
 for (whichont in c("BP", "MF")) {
-  spec_patt <- glob2rx(paste0("GO_top2_*_", whichont, "perm.tsv"))
+  spec_patt <- glob2rx(paste0("GO_top2_*_", whichont, "_perm.tsv"))
   files <- list.files(path = wd, pattern = spec_patt, full.names = T)
   results <- list()
   for (file in files) {
@@ -34,12 +33,11 @@ for (whichont in c("BP", "MF")) {
     summarize(ecdf_frac = list(ecdf(target_frac)),
               ecdf_enrich = list(ecdf(enrich)),
               ecdf_logp = list(ecdf(logp.perm))) %>%
-    saveRDS(file = paste0(wdir, "GO_top2_", whichont, ".robj"))
+    saveRDS(file = paste0(wd, "GO_top2_", whichont, ".robj"))
 }
 
-
 for (whichont in c("BP", "MF")) {
-  spec_patt <- glob2rx(paste0("GO_top3_*_", whichont, "perm.tsv"))
+  spec_patt <- glob2rx(paste0("GO_top3_*_", whichont, "_perm.tsv"))
   files <- list.files(path = wd, pattern = spec_patt, full.names = T)
   results <- list()
   for (file in files) {
@@ -52,5 +50,5 @@ for (whichont in c("BP", "MF")) {
     summarize(ecdf_frac = list(ecdf(target_frac)),
               ecdf_enrich = list(ecdf(enrich)),
               ecdf_logp = list(ecdf(logp.perm))) %>%
-    saveRDS(file = paste0(wdir, "GO_top3_", whichont, ".robj"))
+    saveRDS(file = paste0(wd, "GO_top3_", whichont, ".robj"))
 }
