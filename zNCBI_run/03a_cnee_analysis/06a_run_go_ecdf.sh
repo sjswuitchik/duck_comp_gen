b4546f228831dc1ce42a1889f6862e2acbbc4b45
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-28 10:08:25 -0400

    Update and rename 06a_ecdf_go.sh to 06a_run_go_ecdf.sh

--- zNCBI_run/03a_cnee_analysis/06a_run_go_ecdf.sh
+++ zNCBI_run/03a_cnee_analysis/06a_run_go_ecdf.sh
@@ -1,5 +1,8 @@
 ## in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/postPhyloAcc/go_perms
 
+module load R/4.0.2-fasrc01
+export R_LIBS_USER=$HOME/apps/R_4.0.2
+
 Rscript 
 Rscript
 Rscript
