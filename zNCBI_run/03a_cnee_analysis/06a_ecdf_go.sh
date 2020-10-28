df21ed99134a52f6a683a0a1520519213fe35002
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-28 10:06:06 -0400

    Update 06a_ecdf_go.sh

--- zNCBI_run/03a_cnee_analysis/06a_ecdf_go.sh
+++ zNCBI_run/03a_cnee_analysis/06a_ecdf_go.sh
@@ -1,12 +1,5 @@
 ## in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/postPhyloAcc/go_perms
 
-mkdir top1
-mkdir top2
-mkdir top3
-
-mv GO_top1* top1/
-mv GO_top2* top2/
-mv GO_top3* top3/
-
-cd top1/
 Rscript 
+Rscript
+Rscript
