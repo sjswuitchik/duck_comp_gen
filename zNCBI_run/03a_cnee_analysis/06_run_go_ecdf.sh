c824a43e4a6005534bde8ad12eb5c32efdf0d002
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-28 10:50:48 -0400

    Update 06_run_go_ecdf.sh

--- zNCBI_run/03a_cnee_analysis/06_run_go_ecdf.sh
+++ zNCBI_run/03a_cnee_analysis/06_run_go_ecdf.sh
@@ -4,3 +4,7 @@ module load R/4.0.2-fasrc01
 export R_LIBS_USER=$HOME/apps/R_4.0.2
 
 Rscript go_ecdf.R > std.Rout 2> std.Rerr
+
+Rscript analyze_go_ecdf_top1.R > std.Rout 2> std.Rerr
+Rscript analyze_go_ecdf_top2.R > std.Rout 2> std.Rerr
+Rscript analyze_go_ecdf_top3.R > std.Rout 2> std.Rerr
