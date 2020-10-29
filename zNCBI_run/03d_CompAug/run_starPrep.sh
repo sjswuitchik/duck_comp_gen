efca318adaa0a36e4961b46764ec36c03afa80b7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-10-29 10:28:15 -0400

    Create run_starPrep.sh

---
+++ zNCBI_run/03d_CompAug/run_starPrep.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH -n 12 
+#SBATCH -N 1
+#SBATCH -t 12:00:00
+#SBATCH --mem=64000
+#SBATCH -p shared
+#SBATCH -e starprep.e
+#SBATCH -o starprep.o
+
+module load Anaconda/5.0.1-fasrc01
+source activate compAug
+
+STAR --runMode genomeGenerate --runThreadN ${SLURM_JOB_CPUS_PER_NODE} --genomeDir genome/ --genomeFastaFiles genome/galGal6.fa --sjdbGTFtagExonParentTranscript Parent --genomeSAindexNbases 13
