f086364ea4a5a299b0e72f9c291ce3a8301dbd79
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-11-10 15:03:13 -0500

    Update 05b_go_perms.R

--- zNCBI_run/03a_cnee_analysis/05b_go_perms.R
+++ zNCBI_run/03a_cnee_analysis/05b_go_perms.R
@@ -120,7 +120,36 @@ bp.real.clean <- bp.real@result %>%
 #  dplyr::select(ID, logp, target_frac, bg_frac, OR, enrich) %>%
 #  arrange(ID)
 
-perms.merge <- bp.perms %>% select(-c(geneID, OR))
-real.merge <- bp.real.clean %>% select(-c(geneID, OR))
+bp.perms.merge <- bp.perms %>% 
+  select(-c(geneID, OR)) %>%
+  group_by(ID) %>%
+  summarise(ecdf_frac = list(ecdf(target_frac)),
+            ecdf_enrich = list(ecdf(enrich)),
+            ecdf_logp = list(ecdf(logp)))
 
-left_join(real.merge, perms.merge, by = "ID", suffix = c(".real", ".perm")) %>% View()
+
+bp.real.merge <- bp.real.clean %>% select(-c(geneID, OR))
+
+bp.merge <- left_join(bp.real.merge, bp.perms.merge, by = "ID")
+
+
+# p values from this are all the same ... 
+bp.merge.clean <-
+  bp.merge %>% 
+  rowwise %>% 
+  mutate(pval_frac = max(1-ecdf_frac(target_frac), 0.0002), 
+         pval_logp = max(1-ecdf_logp(logp), 0.0002), 
+         pval_enrich = max(1-ecdf_enrich(enrich), 0.0002)) 
+
+# adjust p values
+pval_frac_adj <- data.frame(pval_frac = bp.merge.clean$pval_frac, qval_frac = p.adjust(bp.merge.clean$pval_frac, "BH"))
+pval_logp_adj <- data.frame(pval_logp = bp.merge.clean$pval_logp, qval_logp = p.adjust(bp.merge.clean$pval_logp, "BH"))
+pval_enrich_adj <- data.frame(pval_enrich = bp.merge.clean$pval_enrich, qval_enrich = p.adjust(bp.merge.clean$pval_enrich, "BH"))
+
+plot(pval_frac_adj$pval_frac, pval_frac_adj$qval_frac)
+
+
+
+
+
+
