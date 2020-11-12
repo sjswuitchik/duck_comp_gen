746fe41b1683a65b76408da71ff5ae0f24965ba6
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-11-12 10:50:24 -0500

    For Nov 12 meeting

--- zNCBI_run/03a_cnee_analysis/05b_go_perms.R
+++ zNCBI_run/03a_cnee_analysis/05b_go_perms.R
@@ -1,4 +1,3 @@
-#### run GO analysis ####
 library(org.Gg.eg.db) # BiocManager::install("org.Gg.eg.db")
 library(tidyverse)
 library(clusterProfiler)
@@ -7,16 +6,9 @@ library(clusterProfiler)
 bg <- read_delim("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/cnee_goperms.counts.bed", delim = "\t", col_names = F) %>%
   rename(chr = X1, start = X2, end = X3, gene = X4, combo = X5, accel = X6, total = X7) %>%
   separate(combo, into = c(NA, "pass1"), sep = "GeneID:", remove = T) %>%
-  separate(pass1, into = c("ncbi", NA), sep = "miRBase:", remove = T) %>%
-  select(-gene)
+  separate(pass1, into = c("ncbi", NA), sep = "miRBase:", remove = T) 
 
-target <- bg %>% filter(accel >= 1) %>% select(-c(chr, start, end, accel, total))
-
-n <- 1000
-prefix <- "perm"
-suffix <- seq(1:n)
-col.names <- paste(prefix, suffix, sep = ".")
-names(target) <- c("ncbi", col.names)
+target <- bg %>% filter(accel >= 1) %>% select(-c(chr, start, end, gene, accel, total))
 
 calc_enrich <- function(targetset, background, ont) { 
   enrichGO(targetset$ncbi,'org.Gg.eg.db',
@@ -29,15 +21,11 @@ calc_enrich <- function(targetset, background, ont) {
 }
 
 bp.perms <- data.frame()
-#mf.perms <- data.frame()
-#cc.perms <- data.frame()
 
-for (i in target[,2:150]) {
+for (i in target[,2:1001]) {
   perm.target <- filter(target, i >= 1) %>%
     select(ncbi)
   bp <- calc_enrich(perm.target, bg, ont = "BP")
-#  mf <- calc_enrich(perm.target, bg, ont = "MF")
-#  cc <- calc_enrich(perm.target, bg, ont = "CC")
   bp.clean <- bp@result %>% 
     separate(GeneRatio, into = c("target_in", "target_total"), sep = '/') %>%
     separate(BgRatio, into = c("bg_in", "bg_total"), sep = '/') %>%
@@ -49,48 +37,16 @@ for (i in target[,2:150]) {
            logp = -log10(newpval)) %>%
     dplyr::select(ID, geneID, logp, target_frac, bg_frac, enrich) %>%
     arrange(ID) 
-#  mf.clean <- mf@result %>% 
-#    separate(GeneRatio, into = c("target_in", "target_total")) %>%
-#    separate(BgRatio, into = c("bg_in", "bg_total")) %>%
-#    mutate(target_frac = as.numeric(target_in)/as.numeric(target_total),
-#           bg_frac = as.numeric(bg_in)/as.numeric(bg_total),
-#           OR = target_frac/bg_frac,
-#           enrich = log2(target_frac/bg_frac),
-#           newpval = ifelse(is.na(pvalue), 1, pvalue),
-#           logp = -log10(newpval)) %>%
-#    dplyr::select(ID, geneID, logp, target_frac, bg_frac, OR, enrich) %>%
-#    arrange(ID) 
-#  cc.clean <- cc@result %>% 
-#    separate(GeneRatio, into = c("target_in", "target_total")) %>%
-#    separate(BgRatio, into = c("bg_in", "bg_total")) %>%
-#    mutate(target_frac = as.numeric(target_in)/as.numeric(target_total),
-#           bg_frac = as.numeric(bg_in)/as.numeric(bg_total),
-#           OR = target_frac/bg_frac,
-#           enrich = log2(target_frac/bg_frac),
-#           newpval = ifelse(is.na(pvalue), 1, pvalue),
-#           logp = -log10(newpval)) %>%
-#    dplyr::select(ID, geneID, logp, target_frac, bg_frac, OR, enrich) %>%
-#    arrange(ID) 
   bp.perms <- as.data.frame(rbind(bp.perms, bp.clean))
-#  mf.perms <- as.data.frame(rbind(mf.perms, mf.clean))
-#  cc.perms <- as.data.frame(rbind(cc.perms, cc.clean))
   
 }
 
-write_tsv(bp.perms, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/bp.perms.tsv", append = F, col_names = T)
-write_tsv(mf.perms, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/mf.perms.tsv", append = F, col_names = T)
-write_tsv(cc.perms, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/cc.perms.tsv", append = F, col_names = T)
-
-bp.perms.load <- read_delim("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/bp.perms.tsv", delim = "\t", ) %>%
-  select(-c(geneID, OR)) %>%
+bp.perms.clean <- bp.perms %>%
+  select(-geneID) %>%
   distinct() %>%
-  arrange(ID)
-#mf.perms.load <- read_delim("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/mf.perms.tsv", delim = "\t", )
-#cc.perms.load <- read_delim("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/cc.perms.tsv", delim = "\t", )
+  arrange()
 
 bp.real <- calc_enrich(target, bg, "BP")
-#mf.real <- calc_enrich(target, bg, "MF")
-#cc.real <- calc_enrich(target, bg, "CC")
 
 bp.real.clean <- bp.real@result %>% 
   separate(GeneRatio, into = c("target_in", "target_total")) %>%
@@ -105,31 +61,14 @@ bp.real.clean <- bp.real@result %>%
   distinct() %>%
   arrange(ID)
 
-#mf.real.clean <- mf.real@result %>% 
-#  separate(GeneRatio, into = c("target_in", "target_total")) %>%
-#  separate(BgRatio, into = c("bg_in", "bg_total")) %>%
-#  mutate(target_frac = as.numeric(target_in)/as.numeric(target_total),
-#         bg_frac = as.numeric(bg_in)/as.numeric(bg_total),
-#         OR = target_frac/bg_frac,
-#         enrich = log2(target_frac/bg_frac),
-#         newpval = ifelse(is.na(pvalue), 1, pvalue),
-#         logp = -log10(newpval)) %>%
-#  dplyr::select(ID, logp, target_frac, bg_frac, OR, enrich) %>%
-#  arrange(ID)
-#
-#cc.real.clean <- cc.real@result %>% 
-#  separate(GeneRatio, into = c("target_in", "target_total")) %>%
-#  separate(BgRatio, into = c("bg_in", "bg_total")) %>%
-#  mutate(target_frac = as.numeric(target_in)/as.numeric(target_total),
-#         bg_frac = as.numeric(bg_in)/as.numeric(bg_total),
-#         OR = target_frac/bg_frac,
-#         enrich = log2(target_frac/bg_frac),
-#         newpval = ifelse(is.na(pvalue), 1, pvalue),
-#         logp = -log10(newpval)) %>%
-#  dplyr::select(ID, logp, target_frac, bg_frac, OR, enrich) %>%
-#  arrange(ID)
-
 bp.merge <- bp.real.clean %>% 
-  left_join(., bp.perms, by = c("ID" = "ID"), suffix = c(".real", ".perm")) %>% 
+  full_join(., bp.perms.clean, by = c("ID" = "ID"), suffix = c(".real", ".perm")) %>% 
   distinct() %>%
   arrange(ID)
+
+  
+  
+  
+  
+  
+  
