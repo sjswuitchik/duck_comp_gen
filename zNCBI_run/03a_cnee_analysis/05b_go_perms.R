b55095bf4209527a295fefdbeeff38d875997f9d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-11-16 12:14:26 -0500

    Update 05b_go_perms.R

--- zNCBI_run/03a_cnee_analysis/05b_go_perms.R
+++ zNCBI_run/03a_cnee_analysis/05b_go_perms.R
@@ -4,13 +4,14 @@ library(tidyverse)
 library(clusterProfiler)
 library(enrichplot)
 
-
+# load in background data and clean up NCBI IDs
 bg <- read_delim("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/cnee_goperms.counts.bed", delim = "\t", col_names = F) %>%
   rename(chr = X1, start = X2, end = X3, gene = X4, combo = X5, accel = X6, total = X7) %>%
   separate(combo, into = c(NA, "pass1"), sep = "GeneID:", remove = T) %>%
   separate(pass1, into = c("ncbi", NA), sep = "miRBase:", remove = T) %>%
   select(-c(chr, start, end, gene, total))
 
+# set up enrichment calculation
 calc_enrich <- function(targetset, background, ont) { 
   enrichGO(targetset$ncbi,'org.Gg.eg.db',
            pvalueCutoff=1.5,
@@ -22,10 +23,12 @@ calc_enrich <- function(targetset, background, ont) {
 }
 
 
+# initialize empty lists for storing permutation results
 bp.perms <- list()
 mf.perms <- list()
 cc.perms <- list()
 
+# loop through every column (each column is a permutation), select target set for given permutation, run through calc_enrich for each subontology, calculate odds ratio and enrichment scores from gene and bg ratios, and store results in list
 for (i in 1:1000) {
   col_name <- paste0("X", i + 7)
   perm.target <- bg %>% filter(.data[[col_name]] >= 1) %>%
@@ -72,12 +75,20 @@ for (i in 1:1000) {
   
 }
 
+# bind permutation results
 bp.perms.clean <- bind_rows(list(bp.perms), .id = "perm")
 mf.perms.clean <- bind_rows(list(mf.perms), .id = "perm")
 cc.perms.clean <- bind_rows(list(cc.perms), .id = "perm")
 
+# write out perms so you don't have to re-run when your session inevitably crashes
+write.delim(bp.perms.clean, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/bp.perms.clean.tsv", delim = "\t", col_names = T)
+write.delim(mf.perms.clean, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/mf.perms.clean.tsv", delim = "\t", col_names = T)
+write.delim(cc.perms.clean, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/cc.perms.clean.tsv", delim = "\t", col_names = T)
+
+# filter out observed target set from background data
 target <- bg %>% filter(accel >= 1)
 
+# calculate enrichment, OR, and enrichment score for real target set for each subontology
 bp.real <- calc_enrich(target, bg, "BP")
 mf.real <- calc_enrich(target, bg, "MF")
 cc.real <- calc_enrich(target, bg, "CC")
@@ -116,6 +127,7 @@ cc.real.clean <- cc.real@result %>%
   dplyr::select(ID, logp, target_frac, bg_frac, enrich) %>%
   arrange(ID)
 
+# merge observed results with permutation results
 bp.merge <- bp.real.clean %>% 
   left_join(., bp.perms.clean, by = c("ID" = "ID"), suffix = c(".real", ".perm")) %>% 
   distinct() %>%
@@ -132,60 +144,52 @@ cc.merge <- cc.real.clean %>%
   arrange(ID) %>%
   select(-perm)
 
+# calculate empirical pvalues (number of instances where real data is greater than permutation)
 bp.p <- bp.merge %>%
   group_by(ID) %>%
-  mutate(gt_bg = bg_frac.perm >= bg_frac.real,
-         gt_target = target_frac.perm >= target_frac.real,
-         gt_enrich = enrich.perm >= enrich.real)
-bp.cols <- sapply(bp.p[,10:12], as.numeric)
+  mutate(gt_target = target_frac.perm <= target_frac.real,
+         gt_enrich = enrich.perm <= enrich.real)
+bp.cols <- sapply(bp.p[,10:11], as.numeric)
 bp.pval <- bp.p %>%
   group_by(ID) %>%
-  mutate(sum_bg = sum(gt_bg) + 1,
-         pVal_bg = sum_bg/1001,
-         sum_target = sum(gt_target) + 1,
+  mutate(sum_target = sum(gt_target) + 1,
          pVal_target = sum_target/1001,
          sum_enrich = sum(gt_enrich) + 1,
          pVal_enrich = sum_enrich/1001) %>%
-  filter(pVal_bg <= 0.05 | pVal_target <= 0.05 | pVal_enrich <= 0.05) %>%
-  select(ID, pVal_bg, pVal_target, pVal_enrich) %>%
+  select(ID, pVal_target, pVal_enrich) %>%
   distinct()
 
 mf.p <- mf.merge %>%
   group_by(ID) %>%
-  mutate(gt_bg = bg_frac.perm >= bg_frac.real,
-         gt_target = target_frac.perm >= target_frac.real,
-         gt_enrich = enrich.perm >= enrich.real)
-mf.cols <- sapply(mf.p[,10:12], as.numeric)
+  mutate(gt_target = target_frac.perm <= target_frac.real,
+         gt_enrich = enrich.perm <= enrich.real)
+mf.cols <- sapply(mf.p[,10:11], as.numeric)
 mf.pval <- mf.p %>%
   group_by(ID) %>%
-  mutate(sum_bg = sum(gt_bg) + 1,
-         pVal_bg = sum_bg/1001,
-         sum_target = sum(gt_target) + 1,
+  mutate(sum_target = sum(gt_target) + 1,
          pVal_target = sum_target/1001,
          sum_enrich = sum(gt_enrich) + 1,
          pVal_enrich = sum_enrich/1001) %>%
-  filter(pVal_bg <= 0.05 | pVal_target <= 0.05 | pVal_enrich <= 0.05) %>%
-  select(ID, pVal_bg, pVal_target, pVal_enrich) %>%
+  select(ID, pVal_target, pVal_enrich) %>%
   distinct()
 
 cc.p <- cc.merge %>%
   group_by(ID) %>%
-  mutate(gt_bg = bg_frac.perm >= bg_frac.real,
-         gt_target = target_frac.perm >= target_frac.real,
-         gt_enrich = enrich.perm >= enrich.real)
-cc.cols <- sapply(cc.p[,10:12], as.numeric)
+  mutate(gt_target = target_frac.perm <= target_frac.real,
+         gt_enrich = enrich.perm <= enrich.real)
+cc.cols <- sapply(cc.p[,10:11], as.numeric)
 cc.pval <- cc.p %>%
   group_by(ID) %>%
-  mutate(sum_bg = sum(gt_bg) + 1,
-         pVal_bg = sum_bg/1001,
-         sum_target = sum(gt_target) + 1,
+  mutate(sum_target = sum(gt_target) + 1,
          pVal_target = sum_target/1001,
          sum_enrich = sum(gt_enrich) + 1,
          pVal_enrich = sum_enrich/1001) %>%
-  filter(pVal_bg <= 0.05 | pVal_target <= 0.05 | pVal_enrich <= 0.05) %>%
-  select(ID, pVal_bg, pVal_target, pVal_enrich) %>%
+  select(ID, pVal_target, pVal_enrich) %>%
   distinct()
 
 write_delim(bp.pval, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/sigterms_BP.tsv", delim = "\t", col_names = T)
 write_delim(mf.pval, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/sigterms_MF.tsv", delim = "\t", col_names = T)
 write_delim(cc.pval, "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_out/NCBI_run/perms_GO/sigterms_CC.tsv", delim = "\t", col_names = T)
+
+
+
