4d11c27236b225b04af935a54891b984bf13487a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-11-20 13:45:12 -0500

    Update concat_fastqs_reseq.sh

--- zNCBI_run/05_CompPopGen/01_fastq2vcf/concat_fastqs_reseq.sh
+++ zNCBI_run/05_CompPopGen/01_fastq2vcf/concat_fastqs_reseq.sh
@@ -17,15 +17,5 @@ cat _______ > stiNae_female_R2.fastq.gz
 
 # hetAtr
 cd ../../hetAtr_v2/fastqs
-
-
-
-
-
-
-
-
-# ind01
-cat hetAtr_ind01_L1_573_1.fastq.gz hetAtr_ind01_L1_574_1.fastq.gz hetAtr_ind01_L1_575_1.fastq.gz hetAtr_ind01_L1_576_1.fastq.gz hetAtr_ind01_L1_577_1.fastq.gz hetAtr_ind01_L1_578_1.fastq.gz hetAtr_ind01_L1_579_1.fastq.gz hetAtr_ind01_L1_580_1.fastq.gz > hetAtr_ind01_1.fastq.gz
-
-cat hetAtr_ind01_L1_573_2.fastq.gz hetAtr_ind01_L1_574_2.fastq.gz hetAtr_ind01_L1_575_2.fastq.gz hetAtr_ind01_L1_576_2.fastq.gz hetAtr_ind01_L1_577_2.fastq.gz hetAtr_ind01_L1_578_2.fastq.gz hetAtr_ind01_L1_579_2.fastq.gz hetAtr_ind01_L1_580_2.fastq.gz > hetAtr_ind01_2.fastq.gz
+cat DGAB-CNB0004-CN4-lib1_S1_L004_R1_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R1_001.fastq.gz > hetAtr_female_R1.fastq.gz
+cat DGAB-CNB0004-CN4-lib1_S1_L004_R2_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R2_001.fastq.gz > hetAtr_female_R2.fastq.gz
