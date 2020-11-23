60a92aca8b70c2ff7d41a798f84af708bf327b83
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-11-23 11:15:38 -0500

    Update concat_fastqs_reseq.sh

--- zNCBI_run/05_CompPopGen/01_fastq2vcf/concat_fastqs_reseq.sh
+++ zNCBI_run/05_CompPopGen/01_fastq2vcf/concat_fastqs_reseq.sh
@@ -2,20 +2,20 @@
 
 # netAur
 cd netAur/fastqs/
-cat netAur_female_L001_R1_001.fastq.gz netAur_female_L004_R1_001.fastq.gz > netAur_female_R1.fastq.gz
-cat netAur_female_L001_R2_001.fastq.gz netAur_female_L004_R2_001.fastq.gz > netAur_female_R2.fastq.gz
+cat netAur_female_L001_R1_001.fastq.gz netAur_female_L004_R1_001.fastq.gz > netAur_female_R.fastq.gz
+cat netAur_female_L001_R2_001.fastq.gz netAur_female_L004_R2_001.fastq.gz > netAur_female_2.fastq.gz
 
 # oxyJam
 cd ../../oxyJam/fastqs
-cat oxyJam_female_L001_R1_001.fastq.gz oxyJam_female_L004_R1_001.fastq.gz > oxyJam_female_R1.fastq.gz
-cat oxyJam_female_L001_R2_001.fastq.gz oxyJam_female_L004_R2_001.fastq.gz > oxyJam_female_R2.fastq.gz
+cat oxyJam_female_L001_R1_001.fastq.gz oxyJam_female_L004_R1_001.fastq.gz > oxyJam_female_1.fastq.gz
+cat oxyJam_female_L001_R2_001.fastq.gz oxyJam_female_L004_R2_001.fastq.gz > oxyJam_female_2.fastq.gz
 
 # stiNae
 cd ../../stiNae/fastqs
-cat stiNae_female_L001_R1_001.fastq.gz stiNae_female_L004_R1_001.fastq.gz > stiNae_female_R1.fastq.gz
-cat stiNae_female_L001_R2_001.fastq.gz stiNae_female_L004_R2_001.fastq.gz > stiNae_female_R2.fastq.gz
+cat stiNae_female_L001_R1_001.fastq.gz stiNae_female_L004_R1_001.fastq.gz > stiNae_female_1.fastq.gz
+cat stiNae_female_L001_R2_001.fastq.gz stiNae_female_L004_R2_001.fastq.gz > stiNae_female_2.fastq.gz
 
 # hetAtr
 cd ../../hetAtr_v2/fastqs
-cat DGAB-CNB0004-CN4-lib1_S1_L004_R1_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R1_001.fastq.gz > hetAtr_female_R1.fastq.gz
-cat DGAB-CNB0004-CN4-lib1_S1_L004_R2_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R2_001.fastq.gz > hetAtr_female_R2.fastq.gz
+cat DGAB-CNB0004-CN4-lib1_S1_L004_R1_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R1_001.fastq.gz > hetAtr_female_1.fastq.gz
+cat DGAB-CNB0004-CN4-lib1_S1_L004_R2_001.fastq.gz DGAB-CNB0004-CN4-lib1_S3_L001_R2_001.fastq.gz > hetAtr_female_2.fastq.gz
