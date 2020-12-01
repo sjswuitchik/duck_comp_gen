4cad0f32ed6624e19cb57af44b8bf30fdfcafa20
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-01 14:14:05 -0500

    Create run_compAug_hints.sh

---
+++ zNCBI_run/03d_CompAug/run_compAug_hints.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p shared
+#SBATCH -e compAug_hints_%A.e
+#SBATCH -o compAug_hints_%A.o
+#SBATCH -J compAug_hints
+#SBATCH --mem=64000
+#SBATCH -t 23:00:00
+
+module purge
+module load Anaconda/5.0.1-fasrc01
+source activate compAug
+
+for ali in mafs/*.maf;
+do
+ id=${ali%.maf} # remove .maf suffix
+ augustus \
+  --species=chicken \
+  --softmasking=1 \
+  --treefile=top1.nwk \
+  --alnfile=$ali \
+  --dbaccess=chicken_rnaseq.db \
+  --speciesfilenames=../genomes.tbl \
+  --alternatives-from-evidence=0 \
+  --dbhints=1 \
+  --UTR=1 \
+  --allow_hinted_splicesites=atac \
+  --extrinsicCfgFile=extrinsic-rnaseq.cfg \
+  --/CompPred/outdir=pred$id > aug$id.out 2> err$id.out &
+done
