18901bf89278ff0127f0c5fcd97055e73aa40f9b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-03 15:01:00 -0500

    Update 02a_run_phyloAcc_waterTop1.sh

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/02a_run_phyloAcc_waterTop1.sh
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/02a_run_phyloAcc_waterTop1.sh
@@ -6,5 +6,5 @@
 #SBATCH --mem 32000
 #SBATCH --array 0-187
 
-source /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/00_setupPhyloAcc.sh
+source /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc_control/PhyloAcc/setupPhyloAcc.sh
 ./PhyloAcc top1_param/run${SLURM_ARRAY_TASK_ID}
