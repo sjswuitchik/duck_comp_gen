8b9ed32468f18194394cae0262a12e093444cf81
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-03 15:00:19 -0500

    Update 00_setupPhyloAcc.sh

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/00_setupPhyloAcc.sh
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/00_setupPhyloAcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module purge
 module load gcc/7.1.0-fasrc01 armadillo/7.800.2-fasrc02 gsl/2.4-fasrc01
-export PATH=/n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/:${PATH}
+export PATH=/n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc_control/PhyloAcc/:${PATH}
