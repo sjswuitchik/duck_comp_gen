fa5833a4bfd16b6766c9419866e2da70ee3c8f0a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-03 14:57:51 -0500

    Update 01a_prepWater_top1.sh

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/01a_prepWater_top1.sh
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/01a_prepWater_top1.sh
@@ -5,11 +5,12 @@ module load gcc/7.1.0-fasrc01 armadillo/7.800.2-fasrc02 gsl/2.4-fasrc01
 mkdir PhyloAcc_control
 cd PhyloAcc_control
 git clone https://github.com/xyz111131/PhyloAcc.git
+cd PhyloAcc
 make
 
-mkdir -p PhyloAcc/input_data
-cd PhyloAcc/input_data
-cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/4d_neut_models/galloTop*.named.mod /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/alignments/aligned/galloseq_ncbi.part.bed /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/alignments/aligned/galloseq_ncbi_gapFixed.fa .
+mkdir input_data
+cd input_data
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/input_data/* .
 cd ..
 
 # run batches of 2000 elements each
@@ -31,9 +32,9 @@ do
   PARTFILE=top1_batches/batch$BATCH
   PREFIX=batch${BATCH}
   cat > top1_param/run$I <<EOF 
-PHYTREE_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/input_data/galloTop1.named.mod
-SEG_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/input_data/galloseq_ncbi.part.bed
-ALIGN_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc/input_data/galloseq_ncbi_gapFixed.fa
+PHYTREE_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc_control/PhyloAcc/input_data/galloTop1.named.mod
+SEG_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc_control/PhyloAcc/input_data/galloseq_ncbi.part.bed
+ALIGN_FILE /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis/PhyloAcc_control/PhyloAcc/input_data/galloseq_ncbi_gapFixed.fa
 RESULT_FOLDER top1_outs
 PREFIX $PREFIX
 ID_FILE $PARTFILE
@@ -46,8 +47,8 @@ ACCE_PRIOR_A 10
 ACCE_PRIOR_B 0.2
 HYPER_GRATE_A 3
 HYPER_GRATE_B 1
-TARGETSPECIES hetAtr
-CONSERVE netAur;stiNae;oxyJam;anaPla;braCan;ansInd;ansCyg;ansBra;numMel;colVir;tymCupPin;syrMik;galGal;cotJap
+TARGETSPECIES hetAtr;netAur;stiNae;oxyJam;anaPla;braCan;ansInd;ansCyg;ansBra
+CONSERVE numMel;colVir;tymCupPin;syrMik;galGal;cotJap
 GAPCHAR -
 NUM_THREAD 8
 VERBOSE 0
