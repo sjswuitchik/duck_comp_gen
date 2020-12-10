ff3a86ed1fdb0307cd74981e2e0593fcb98b36d6
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-10 12:48:45 -0500

    Update 06_cleanup_cnees_phyloP.R

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/06_cleanup_cnees_phyloP.R
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/06_cleanup_cnees_phyloP.R
@@ -1,7 +1,7 @@
 # remove all CNEEs that don't show strong evidence for conservation in Galloanserae from phyloP under all topologies after FDR correction
 
 library(tidyverse)
-setwd("~/Desktop/PDF/duck_assemblies/PhyloAcc_out/NCBI_run") #I know, sorry!
+setwd("~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_control") 
 
 # read in all phyloP outputs for topologies, calculate FDR, create list of CNEEs to exclude
 top1 <- read_delim("cnees_ncbi_phyloP_top1.out", delim = "\t", col_names = T) %>%
@@ -38,11 +38,13 @@ acc <- acc.cnee %>%
   select(cnee)
 
 # intersect acc cnees with excl cnees to see if any need to be removed 
-final.excl <- intersect(acc, cnee.excl.alltop) #zfCNEE300947
+excl <- intersect(acc, cnee.excl.alltop) #84 total
+# create vector of CNEE names to be removed
+excl.vec <- pull(excl, cnee) 
 
-# filter out the one CNEE - would need to filter/setdiff based on final.excl object if more than a couple CNEEs
-final.cnee <- acc.cnee %>%
-  filter(cnee != "zfCNEE300947")
+# filter out the CNEEs without evidence for conservation using an anti-join
+excl.cnee <- subset(acc.cnee, cnee %in% excl.vec)
+final.cnee <- anti_join(acc.cnee, excl.cnee, by = "cnee")
 
 # write out new acc.cnees BED file
 write_delim(final.cnee, "acc.cnees.final.bed", delim = "\t", col_names = F)
