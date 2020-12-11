3a58d26a7aeb0f836e12ba2af47ed242f43053fb
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-12-11 10:46:12 -0500

    Update 08a_cnee_perms.R

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/08a_cnee_perms.R
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_control/08a_cnee_perms.R
@@ -1,4 +1,3 @@
-#### new perms using shuf #### 
 library(RColorBrewer)
 library(ggrepel)
 library(tidyverse)
@@ -9,14 +8,8 @@ data.clean <- bind_cols(data.convert, data) %>%
   select(-c(X4)) %>%
   rename(X4 = ...1) %>%
   select(X1:X3, X4, X5:X1006)
-# calculating pVal this way is off by an order of magnitude .. sum(X7:X1006) isn't working the way I want it to, but count() and n() don't work either and sumRows() will add the actual numbers (e.g. if perm = 2 and is gt obs, then will be summed as 2 perms gt obs instead of one perm)
-pVal <- data.clean %>%  
-  rowwise() %>%
-  mutate(pVal = sum((X7:X1006 >= X5)+1)/(1000+1)) %>%
-  select(X1:X6, pVal) %>%
-  rename(chr = X1, start= X2, end = X3, gene = X4, accel = X5, total = X6)
 
-# try calculating pVal this way instead - set logical TRUE for count > obs, convert logical to numerical, then sum per gene 
+# calculating pVals - set logical TRUE for count > obs, convert logical to numerical, then sum per gene 
 t <- data.clean %>% 
   select(X4, X5, X6, X7:X1006) %>% 
   rename(gene = X4, obs = X5, total = X6) %>% 
@@ -36,36 +29,27 @@ tidyt <- tpval %>%
 
 # Adjust p old school
 pv <- data.frame(adjP = p.adjust(tidyt$pVal, method = "fdr"), pVal = tidyt$pVal)
-# plot to make sure adjustment worked (ie/ not a 1:1 line)
+# plot to make sure adjustment worked (ie/ not an exact 1:1 line)
 plot(-log10(pv$adjP), -log10(pv$pVal))
 # only keep the adjusted p-values
 pv <- pv %>% select(-c(pVal))
 adjP <- bind_cols(tidyt, pv) %>%
   mutate(sig_class = case_when(
     adjP <= 0.05 ~ "Enriched for accelerated regions (5% FDR)",
-    adjP > 0.05 ~ "Not enriched"))
+    adjP > 0.05 ~ "Not enriched")) 
 
+# one gene that has 190 acc'd CNEEs - check it out
+adjP %>% filter(obs == 190) %>% View() # CELF4 - 190 acc, 880 total
 
-ggplot(adjP, aes(x = obs, y = total, col = sig_class, label = gene)) +
+ggplot(test, aes(x = obs, y = total, col = sig_class, label = gene)) +
   theme_classic() + 
   scale_y_log10() + 
   geom_jitter(shape = 16) +
   scale_colour_brewer(palette = "Dark2") +
   labs(x = "Number of accelerated CNEEs near gene", y = "Total number of CNEEs near gene", color = "Significance") + 
-  scale_x_continuous(breaks = c(1,2,3,4,5)) #+
-  geom_label_repel(data = subset(adjP, adjP <= 0.05),
-                   aes(label = gene),
-                   box.padding = 1.5,
-                   point.padding = 0.5,
-                   segment.size = 0.2,
-                   force = 100,
-                   segment.colour = 'grey50')
+  scale_x_continuous() 
 
-  
-geneList <- adjP %>% filter(adjP < 0.05) %>% select(gene) %>% write_delim(., "~/Desktop/PDF/CNEEs/PhyloAcc_control/geneList.txt", delim = "\t", col_names = T)
-
-# two significant CNEEs with only 1 obs
-adjP %>% filter(adjP < 0.05) %>% filter(obs == 1) %>% View()
+geneList <- adjP %>% filter(adjP < 0.05) %>% select(gene) %>% write_delim(., "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_control/geneList.txt", delim = "\t", col_names = T)
 
 
 
@@ -84,4 +68,4 @@ collapse <- martList %>%
 sub1 <- collapse[!(is.na(collapse$goID) | collapse$goID == ""), ] 
 clean.mart <- sub1[-1,]
 
-left_join(geneList, clean.mart, by = "gene") %>% write_delim(., "~/Desktop/PDF/CNEEs/PhyloAcc_control/martList.tsv", delim = "\t", col_names = T)
+left_join(geneList, clean.mart, by = "gene") %>% write_delim(., "~/Desktop/PDF/duck_assemblies/CNEEs/PhyloAcc_control/martList.tsv", delim = "\t", col_names = T)
