9663932682dc5aa25f81441d270bcb7ab97e76bd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-05 10:21:57 -0500

    Update 01_prep_orthoFinder.sh

--- NCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
+++ NCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
@@ -4,18 +4,17 @@
 
 module load Anaconda3/2019.10
 
-# from /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/04_OrthoFinder
+# from /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/04a_OrthoFinder_compAug
 
 #conda create -c bioconda -n ortho orthofinder diamond 
 #conda install -c conda-forge perl
 conda activate ortho
 
 mkdir -p run_ortho/input_data
-cd run_ortho/input_data
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/011/077/185/GCF_011077185.1_BPBGC_Ojam_1.0/GCF_011077185.1_BPBGC_Ojam_1.0_protein.faa.gz
-gunzip *.gz
-cp /n/holyscratch01/informatics/swuitchik/ducks_project/post_cactus/orthos/run_ortho/input_data/galGal.translated.fa .
-cd ../..
+for file in hetAtr stiNae oxyJam netAur;
+do
+  cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03d_CompAug/busco/input_data/$file.translated.fa run_ortho/input_data/
+done
 
 # run OrthoFinder 
 sbatch 02_ncbi_ortho.sh
