94bc4fb1619857b6c7319d48a536201478242850
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-05 14:39:09 -0500

    Update 01_prep_orthoFinder.sh

--- NCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
+++ NCBI_run/04_OrthoFinder/01_prep_orthoFinder.sh
@@ -8,13 +8,17 @@ module load Anaconda3/2019.10
 
 #conda create -c bioconda -n ortho orthofinder diamond 
 #conda install -c conda-forge perl
-conda activate ortho
 
 mkdir -p run_ortho/input_data
-for file in hetAtr stiNae oxyJam netAur;
+# get ducks & requisite outgroup protein fastas
+for file in hetAtr stiNae oxyJam netAur galGal;
 do
   cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03d_CompAug/busco/input_data/$file.translated.fa run_ortho/input_data/
 done
+# grab mallard GFF and translate to protein fasta
+cp -v /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03d_CompAug/augCGP_rnahints/joined_pred/anaPla.gff .
+source activate busco
+gffread anaPla.gff -g /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03d_CompAug/genomes/anaPla.ncbi.fasta -y run_ortho/input_data/anaPla.translated.fa -S
 
 # run OrthoFinder 
 sbatch run_orthoFinder.sh
