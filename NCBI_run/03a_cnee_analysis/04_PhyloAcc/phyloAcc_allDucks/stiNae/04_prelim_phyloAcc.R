a1a0b3c6981f134280543a1e95ab299402b21f6b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-07 10:19:16 -0500

    Update and rename prelim_phyloAcc.R to 04_prelim_phyloAcc.R

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_allDucks/stiNae/04_prelim_phyloAcc.R
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_allDucks/stiNae/04_prelim_phyloAcc.R
@@ -156,3 +156,51 @@ bed <- accelerated %>%
   select(chr, newstart, end, cnee) %>% 
   rename('#chrom' = chr, chromStart = newstart, chromEnd = end, name = cnee)
 write_delim(bed, "acc_cnees.bed", delim = '\t')
+
+#### clean up CNEEs by excluding any CNEEs that show no evidence for conservation across all three topologies (from phyloP output)
+
+# read in all phyloP outputs for topologies, calculate FDR, create list of CNEEs to exclude
+top1 <- read_delim("cnees_ncbi_phyloP_top1.out", delim = "\t", col_names = T) %>%
+  rename(cnee = '#chr') %>%
+  mutate(fdr = p.adjust(pval, method = "fdr"),
+         excl = fdr < 0.10)
+top1.excl <- top1 %>% 
+  filter(excl == F) %>%
+  select(cnee)
+
+top2 <- read_delim("cnees_ncbi_phyloP_top2.out", delim = "\t", col_names = T) %>%
+  rename(cnee = '#chr') %>%
+  mutate(fdr = p.adjust(pval, method = "fdr"),
+         excl = fdr < 0.10)
+top2.excl <- top2 %>% 
+  filter(excl == F) %>%
+  select(cnee)
+
+top3 <- read_delim("cnees_ncbi_phyloP_top3.out", delim = "\t", col_names = T) %>%
+  rename(cnee = '#chr') %>%
+  mutate(fdr = p.adjust(pval, method = "fdr"),
+         excl = fdr < 0.10)
+top3.excl <- top3 %>% 
+  filter(excl == F) %>%
+  select(cnee)
+
+# intersect exclusionary CNEE lists for all topologies
+cnee.excl.alltop <- intersect(top1.excl, top2.excl, top3.excl)
+
+# get list of accelerated CNEEs from BED
+acc <- bed %>%
+  select(cnee)
+
+# intersect acc cnees with excl cnees to see if any need to be removed 
+excl <- intersect(acc, cnee.excl.alltop) #84 total
+# create vector of CNEE names to be removed
+excl.vec <- pull(excl, cnee) 
+
+# filter out the CNEEs without evidence for conservation using an anti-join
+excl.cnee <- subset(acc.cnee, cnee %in% excl.vec)
+final.cnee <- anti_join(acc.cnee, excl.cnee, by = "cnee")
+
+# write out new acc.cnees BED file
+write_delim(final.cnee, "acc.cnees.final.bed", delim = "\t", col_names = F)
+
+
