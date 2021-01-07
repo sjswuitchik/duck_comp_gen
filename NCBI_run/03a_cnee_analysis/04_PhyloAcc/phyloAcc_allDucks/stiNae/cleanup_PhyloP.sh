3ed8fe14de757e4eb2aefe22f242b9e53d8faed8
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-07 10:12:51 -0500

    Create cleanup_PhyloP.sh

---
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_allDucks/stiNae/cleanup_PhyloP.sh
@@ -0,0 +1,11 @@
+module load Anaconda3/2019.10
+
+# in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/phyloAcc_allDucks/stiNae/PhyloAcc
+
+conda activate phast 
+
+# generate data to use in phyloP_cleanup_cnees.R to identify any CNEEs that show no evidence of conservation in all three topologies
+# usage: phyloP [OPTIONS] tree.mod [alignment] > out
+phyloP --method LRT --mode CON --msa-format FASTA --features input_data/galloseq_ncbi.part.bed input_data/galloTop1.named.mod input_data/galloseq_ncbi_gapFixed.fa > cnees_ncbi_phyloP_top1.out
+phyloP --method LRT --mode CON --msa-format FASTA --features input_data/galloseq_ncbi.part.bed input_data/galloTop2.named.mod input_data/galloseq_ncbi_gapFixed.fa > cnees_ncbi_phyloP_top2.out
+phyloP --method LRT --mode CON --msa-format FASTA --features input_data/galloseq_ncbi.part.bed input_data/galloTop3.named.mod input_data/galloseq_ncbi_gapFixed.fa > cnees_ncbi_phyloP_top3.out
