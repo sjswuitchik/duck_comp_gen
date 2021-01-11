b51f21d11584590d09c7927d312c3babaffa6ef6
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-11 10:24:30 -0500

    Update 05_postPhyloAcc.sh

--- NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_allDucks/stiNae/05_postPhyloAcc.sh
+++ NCBI_run/03a_cnee_analysis/04_PhyloAcc/phyloAcc_allDucks/stiNae/05_postPhyloAcc.sh
@@ -8,17 +8,11 @@ cp /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/03_cnees/galGal6_fi
 # sort full final list
 bedtools sort -i galGal6_final_merged_CNEEs_named.bed > galGal6_final_merged_CNEEs_named_sorted.bed
 
-# make galGal6 chrom.sizes file for bedtools makewindows with faToTwoBit and twoBitInfo
-wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faToTwoBit
-chmod +x ./faToTwoBit
-wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/twoBitInfo
-chmod +x ./twoBitInfo
-
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.6_GRCg6a/GCF_000002315.6_GRCg6a_genomic.fna.gz
 gunzip GCF_000002315.6_GRCg6a_genomic.fna.gz
 
-./faToTwoBit GCF_000002315.6_GRCg6a_genomic.fna galGal.fa.2bit
-./twoBitInfo galGal.fa.2bit stdout | sort -k2rn > galGal.chrom.sizes
+.././faToTwoBit GCF_000002315.6_GRCg6a_genomic.fna galGal.fa.2bit
+.././twoBitInfo galGal.fa.2bit stdout | sort -k2rn > galGal.chrom.sizes
 
 # create 100kb windows with 50kb slide
 bedtools makewindows -g galGal.chrom.sizes -w 100000 -s 50000 > galGal.windows.bed
@@ -28,22 +22,9 @@ bedtools intersect -a galGal.windows.bed -b galGal6_final_merged_CNEEs_named_sor
 bedtools intersect -a galGal.windows.bed -b acc.cnees.final.bed -loj | cut -f1,2,3,7 | sed --expression='s/\.$/0/g' > window.acc.cnees.bed
 
 ### input generation for the assessment of genes with evidence for excess of nearby accelerated CNEEs
-# get galGal6 annotation
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.6_GRCg6a/GCF_000002315.6_GRCg6a_genomic.gff.gz
-gunzip GCF_000002315.6_GRCg6a_genomic.gff.gz
-
-# gff to bed
-column -s, -t < GCF_000002315.6_GRCg6a_genomic.gff | awk '$3 == "gene"' > galGal.genes.gff
-awk -f gff2bed.awk galGal.genes.gff > galGal.gff.bed
-
-# pull out genes
-cat galGal.gff.bed | python3 genenames.py > galGal.genes.bed
-
-# sort 
-bedtools sort -i galGal.genes.bed > galGal.genes.sorted.bed
 
 # set 100 kb window around genes
-bedtools slop -i galGal.genes.sorted.bed -g galGal.chrom.sizes -b 100000 > galGal.slop.bed
+bedtools slop -i ../galGal.genes.sorted.bed -g galGal.chrom.sizes -b 100000 > galGal.slop.bed
 
 # sort full list of CNEEs
 bedtools sort -i galGal6_final_merged_CNEEs_named.bed > galGal6_final_merged_CNEEs_named_sorted.bed
@@ -67,7 +48,7 @@ bedtools annotate -i cnee_gene100kb.clean.bed -files cnee_perms/*.bed -counts > 
 # nb: could also write quick script (like replace_chr.pl) to replace the gene symbols with NCBI gene IDs in cnee_perms.counts.bed
 # pull out gene symbols & NCBI IDs
 mkdir go_perms
-cat galGal.gff.bed | python3 gene_ncbi_names.py > go_perms/galGal.ncbigenes.bed
+cat galGal.gff.bed | python3 ../gene_ncbi_names.py > go_perms/galGal.ncbigenes.bed
 cd go_perms/
 
 # sort 
