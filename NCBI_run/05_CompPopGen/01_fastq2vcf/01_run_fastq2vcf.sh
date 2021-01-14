7eb5cef169f50c17d2e765d44bcad26b49653b54
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-14 13:33:51 -0500

    Update 01_run_fastq2vcf.sh

--- NCBI_run/05_CompPopGen/01_fastq2vcf/01_run_fastq2vcf.sh
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/01_run_fastq2vcf.sh
@@ -1,7 +1,7 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/01_fastq2vcf/
 
 module load Anaconda/5.0.1-fasrc01 bcftools/1.5-fasrc02
-#conda create --name snakemake -c bioconda snakemake samtools picard
+#conda create --name snakemake -c bioconda snakemake samtools picard gatk4
 
 # clone pipeline & repo 
 git clone https://github.com/harvardinformatics/shortRead_mapping_variantCalling
