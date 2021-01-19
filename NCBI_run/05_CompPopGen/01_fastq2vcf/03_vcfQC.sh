ad998acd20d5cc87b88cd3901d554516c0f760a2
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-19 14:31:49 -0500

    Create 03_vcfQC.sh

---
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/03_vcfQC.sh
@@ -0,0 +1,5 @@
+# in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/01_fastq2vcf/vcfs/
+
+module load bcftools/1.5-fasrc02 bedtools2/2.26.0-fasrc01 perl/5.26.1-fasrc01
+
+cp ../shortRead_mapping_variantCalling/gatk/Combined_hardFiltered.vcf .
