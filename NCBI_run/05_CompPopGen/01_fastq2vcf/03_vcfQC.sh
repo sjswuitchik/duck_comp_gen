a7cec313fb6b8ec92e1977f10d5d3791302bbda1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-19 14:49:04 -0500

    Update 03_vcfQC.sh

--- NCBI_run/05_CompPopGen/01_fastq2vcf/03_vcfQC.sh
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/03_vcfQC.sh
@@ -1,5 +1,11 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/05b_comppopgen_snakemake/01_fastq2vcf/vcfs/
 
-module load bcftools/1.5-fasrc02 bedtools2/2.26.0-fasrc01 perl/5.26.1-fasrc01
+module load bcftools/1.5-fasrc02
 
 cp ../shortRead_mapping_variantCalling/gatk/Combined_hardFiltered.vcf .
+cp ~/github_repos/duck_comp_gen/NCBI_run/05_CompPopGen/02_MKpipeline/hetAtr_indvs .
+
+bcftools view -O z -s stiNae_male Combined_hardFiltered.vcf > stiNae.vcf.gz
+bcftools view -O z -S hetAtr_indvs Combined_hardFiltered.vcf > hetAtr.vcf.gz
+bcftools view -O z -s netAur_male Combined_hardFiltered.vcf > netAur.vcf.gz
+bcftools view -O z -s oxyJam_male Combined_hardFiltered.vcf > oxyJam.vcf.gz
