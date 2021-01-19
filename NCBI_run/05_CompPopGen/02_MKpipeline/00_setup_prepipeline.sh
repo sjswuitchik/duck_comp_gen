c4670f73a83193e7846e4e5136e1d0037652f86f
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-19 14:52:14 -0500

    Update 00_setup_prepipeline.sh

--- NCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
+++ NCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
@@ -12,7 +12,7 @@ bcftools view -O z -s stiNae_male Combined_hardFiltered.vcf > stiNae.vcf.gz
 bcftools view -O z -S hetAtr_indvs Combined_hardFiltered.vcf > hetAtr.vcf.gz
 
 # separate hetAtr and stiNae missingness data
-grep -v stiNae_ind01 missing_data_per_ind.txt > hetAtr_missing_data.txt
+grep -v stiNae_male missing_data_per_ind.txt > hetAtr_missing_data.txt
 sed -n 1p missing_data_per_ind.txt > stiNae_missing_data.txt
 grep -v -f hetAtr_missing_data.txt missing_data_per_ind.txt > temp
 cat temp >> stiNae_missing_data.txt
