907100eaf3b5bcec927fd91cd3e8584292b27f08
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-01-19 14:41:00 -0500

    Update 00_setup_prepipeline.sh

--- NCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
+++ NCBI_run/05_CompPopGen/02_MKpipeline/00_setup_prepipeline.sh
@@ -8,7 +8,7 @@ cp gatk/Combined_hardFiltered.vcf manual/missing_data_per_ind.txt ../../02_MK_pi
 cd ../../02_MK_pipeline
 
 # separate hetAtr and stiNae VCFs
-bcftools view -O z -s stiNae_ind01 Combined_hardFiltered.vcf > stiNae.vcf.gz
+bcftools view -O z -s stiNae_male Combined_hardFiltered.vcf > stiNae.vcf.gz
 bcftools view -O z -S hetAtr_indvs Combined_hardFiltered.vcf > hetAtr.vcf.gz
 
 # separate hetAtr and stiNae missingness data
