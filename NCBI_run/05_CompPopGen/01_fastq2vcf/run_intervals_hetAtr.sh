e722995f74867b2d0511ebf04d771d9f81e5d501
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-02-24 11:09:40 -0500

    Update run_intervals_hetAtr.sh

--- NCBI_run/05_CompPopGen/01_fastq2vcf/run_intervals_hetAtr.sh
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/run_intervals_hetAtr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -J sm
-#SBATCH -o out
-#SBATCH -e err
+#SBATCH -J sm_hetAtr
+#SBATCH -o out_hetAtr
+#SBATCH -e err_hetAtr
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 9000
 #SBATCH --mem=10000
 
-
+module load Anaconda3/2020.11
 source activate snakemake
-snakemake --snakefile Snakefile_intervals --profile ./profiles/slurm
+snakemake --snakefile Snakefile_intervals_hetAtr --profile ./profiles/slurm
 
