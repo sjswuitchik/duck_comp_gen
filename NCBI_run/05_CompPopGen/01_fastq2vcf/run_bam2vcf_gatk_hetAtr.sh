6783f0af85475c236e282e738016c2c883332d7a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-02-24 11:05:12 -0500

    Update run_bam2vcf_gatk_hetAtr.sh

--- NCBI_run/05_CompPopGen/01_fastq2vcf/run_bam2vcf_gatk_hetAtr.sh
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/run_bam2vcf_gatk_hetAtr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J sm
-#SBATCH -o out
-#SBATCH -e err
+#SBATCH -J sm_hetAtr
+#SBATCH -o out_hetAtr
+#SBATCH -e err_hetAtr
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 9000
@@ -9,5 +9,5 @@
 
 module load Anaconda3/2020.11
 source activate snakemake
-snakemake --snakefile Snakefile_bam2vcf_gatk --profile ./profiles/slurm
+snakemake --snakefile Snakefile_bam2vcf_gatk_hetAtr --profile ./profiles/slurm
 
