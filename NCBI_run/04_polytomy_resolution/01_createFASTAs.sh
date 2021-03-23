b13e007e29bcf80c992997cfdc2ec5f0b504844d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-23 16:20:10 -0400

    Update 01_createFASTAs.sh

--- NCBI_run/04_polytomy_resolution/01_createFASTAs.sh
+++ NCBI_run/04_polytomy_resolution/01_createFASTAs.sh
@@ -1,5 +1,9 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee
 
+# copy over CNEE data
+cp /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/03_cnees/hetAtr/galGal6_final_merged_CNEEs_named.bed  /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/03_cnees/hetAtr/all_cnees.tab .
+
+
 #conda create -n r -c bioconda r-base r-tidyverse
 source activate r
-Rscript 
+Rscript cnee_keys.R
