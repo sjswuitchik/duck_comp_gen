f170682eed9549e53b4b457e4cc789d1f8509fb2
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-03-29 10:32:05 -0400

    Create 03_mafftAlign.sh

---
+++ NCBI_run/04_polytomy_resolution/03_mafftAlign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J mafftAlign
+#SBATCH -o logs/slurm-%j
+#SBATCH -e logs/slurm-%j
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH --mem=8000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee
+
+conda activate align
+
+for file in fastas/*.fa;
+do
+  mafft --quiet $file > $file.mafft
+done
