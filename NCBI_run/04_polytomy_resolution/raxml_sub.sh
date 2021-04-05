c498f737800327a606402dc908dd2bebe70d66bf
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-05 15:51:39 -0400

    Update raxml_sub.sh

--- NCBI_run/04_polytomy_resolution/raxml_sub.sh
+++ NCBI_run/04_polytomy_resolution/raxml_sub.sh
@@ -4,8 +4,8 @@
 #SBATCH -e logs/slurm-%j
 #SBATCH -p shared
 #SBATCH -n 1
-#SBATCH -t 24:00:00
-#SBATCH --mem=8000
+#SBATCH -t 00-03:00:00
+#SBATCH --mem=4000
 
 # submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee
 
