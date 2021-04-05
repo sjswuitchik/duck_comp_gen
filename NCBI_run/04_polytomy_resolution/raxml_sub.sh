5ea80c8d9405365384e71d8cab7c140e8885ca21
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-05 15:50:20 -0400

    Create raxml_sub.sh

---
+++ NCBI_run/04_polytomy_resolution/raxml_sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J RAxMLsub
+#SBATCH -o logs/slurm-%j
+#SBATCH -e logs/slurm-%j
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH --mem=8000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee
+
+for file in fastas/muscle/subset/*.afa;
+do
+  raxml-ng --msa $file --model HKY+G4 --prefix T --seed 2
+done
