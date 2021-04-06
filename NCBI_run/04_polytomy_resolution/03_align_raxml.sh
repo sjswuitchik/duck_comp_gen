a01a27a669faf34660fcdd3b0e0f52a164a2ae96
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-06 10:09:54 -0400

    Update 03_align_raxml.sh

--- NCBI_run/04_polytomy_resolution/03_align_raxml.sh
+++ NCBI_run/04_polytomy_resolution/03_align_raxml.sh
@@ -4,33 +4,30 @@
 sbatch mafftAlign.sh
 sbatch muscleAlign.sh
 
+##### CNEE alignments from MUSCLE were manually trimmed to remove sites where indels where present in >1 spp
+
 # rename CNEE alignments to deal with that one stupid parsing mistake from way back
 #conda create -n raxml -c bioconda raxml-ng modeltest-ng rename 
 source activate raxml 
-for file in fastas/muscle/*.afa;
-do
-  rename -d zf $file
-done
-
-for file in fastas/mafft/*.mafft;
+for file in trimmed/*.fa;
 do
   rename -d zf $file
 done
 
 # model selection for RAxML-NG
-mkdir -p fastas/muscle/subset
-cd fastas/muscle/
+mkdir -p trimmed/subset
+cd trimmed/
 for file in $(ls -p | grep -v / | tail -50)
 do
   cp $file subset/
 done
 
-for file in subset/*.fa.afa;
+for file in subset/*.fa;
 do
   modeltest-ng -d nt -i $file -o $file -T raxml
 done
 
-for file in subset/*.afa;
+for file in subset/*.fa;
 do
   raxml-ng --check --msa $file --model HKY+G4 --prefix tcheck_$file
 done
