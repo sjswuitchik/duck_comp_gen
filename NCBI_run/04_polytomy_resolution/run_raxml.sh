1630870d2ec393571ff8e9b434d9bff451dfb69e
sjswuitchik <sjswuit@g.harvard.edu>
2021-04-06 11:49:48 -0400

    Updated run_raxml.sh

--- NCBI_run/04_polytomy_resolution/run_raxml.sh
+++ NCBI_run/04_polytomy_resolution/run_raxml.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -J RAxMLsub
+#SBATCH -J RAxML
 #SBATCH -o logs/slurm-%j.out
 #SBATCH -e logs/slurm-%j.err
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 00-24:00:00
-#SBATCH --mem=4000
+#SBATCH --mem=8000
 
 # submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee
 
 for file in trimmed/subset/*.fa;
 do
-  raxml-ng --msa $file --model HKY+G4 --prefix $file
+  raxml-ng --redo --msa $file --model HKY+G4 --prefix $file
 done
