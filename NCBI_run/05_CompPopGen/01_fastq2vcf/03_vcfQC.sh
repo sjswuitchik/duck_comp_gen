cbadf5d5f167b017cc6ec726185eedda91f5ad96
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-14 13:54:50 -0400

    Update 03_vcfQC.sh

--- NCBI_run/05_CompPopGen/01_fastq2vcf/03_vcfQC.sh
+++ NCBI_run/05_CompPopGen/01_fastq2vcf/03_vcfQC.sh
@@ -26,7 +26,7 @@ java -jar DISCVRSeq.jar VariantQC --maxContigs 26704 -R hetAtr.fa -V stiNae.filt
 
 conda deactivate 
 
-#conda create -n vcfqc -c bioconda plink vcftools bcftools r-base r-tidyverse admixture perl bedtools angsd
+#conda create -n vcfqc -c bioconda plink vcftools bcftools r-base r-tidyverse admixture perl bedtools angsd samtools
 source activate vcfqc
 # output stats
 vcftools --gzvcf hetAtr.filtered.vcf.gz --out hetAtr.rel --relatedness2
@@ -72,9 +72,11 @@ rm stairway_plot_v2.1.1.zip
 mv stairway-plot-v2/ stairway/
 cp ../hetAtr.filtered.vcf .
 cp ../hetAtr_indvs .
-vcftools --vcf hetAtr.filtered.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --out hetAtr.stair --recode --recode-INFO-all
-
-
+cp /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/03_CompAugAnnotation/genomes/hetAtr.ncbi.fasta .
+samtools faidx hetAtr.ncbi.fasta -o hetAtr.ncbi.fasta.fai
+vcftools --vcf hetAtr.filtered.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --remove-indels --out hetAtr.stair --recode --recode-INFO-all
+angsd -vcf-gl hetAtr.stair.recode.vcf -doSaf 1 -fold 1 -out hetAtr -anc hetAtr.ncbi.fasta # try folding here
+realSFS hetAtr.saf.idx -fold 1 > hetAtr.folded # or here
 
 
 
