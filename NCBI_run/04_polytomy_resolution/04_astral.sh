a1df9cb675a38f995a792896cd24817a03426ec4
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-19 11:17:50 -0400

    Update 04_astral.sh

--- NCBI_run/04_polytomy_resolution/04_astral.sh
+++ NCBI_run/04_polytomy_resolution/04_astral.sh
@@ -12,3 +12,11 @@ cp ../trimmed/final.tree .
 
 # run ASTRAL 
 java -jar astral.5.7.7.jar -i final.tree -o final.astral.tree
+
+# copy single copy ortho gene trees over from OrthoFinder output to /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee/orthofinder_geneTrees
+sed -i.bak 's/_[^:]\{1,\}:/:/g' *.txt
+awk '{print}' *_tree.txt > gene_trees.txt
+cp gene_trees ../Astral
+cd ../Astral
+java -jar astral.5.7.7.jar -i gene_trees.txt -o ortho.tree 2> log
+
