86067245a3f7431a408dc92869e86ad7f3d7c874
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-21 10:09:07 -0400

    Update angsd_stairway.sh

--- NCBI_run/05_CompPopGen/03_popGen/angsd_stairway.sh
+++ NCBI_run/05_CompPopGen/03_popGen/angsd_stairway.sh
@@ -13,6 +13,9 @@ samtools faidx hetAtr.ncbi.fasta -o hetAtr.ncbi.fasta.fai
 
 sbatch run_angsd.sh
 
+# run Stairbuilder
+mkdir plots
+
 
 
 vcftools --vcf hetAtr.filtered.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --remove-indels --out hetAtr.stair --recode --recode-INFO-all
