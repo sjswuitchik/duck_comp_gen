c8abcf8c3042a8bf6ede1a03ccbf96c4808b1952
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-21 10:05:21 -0400

    Update angsd_stairway.sh

--- NCBI_run/05_CompPopGen/03_popGen/angsd_stairway.sh
+++ NCBI_run/05_CompPopGen/03_popGen/angsd_stairway.sh
@@ -13,8 +13,6 @@ sbatch run_angsd.sh
 
 
 cp ../hetAtr.filtered.vcf .
-cp ../hetAtr_indvs .
-cp /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/03_CompAugAnnotation/genomes/hetAtr.ncbi.fasta .
 samtools faidx hetAtr.ncbi.fasta -o hetAtr.ncbi.fasta.fai
 vcftools --vcf hetAtr.filtered.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --remove-indels --out hetAtr.stair --recode --recode-INFO-all
 
