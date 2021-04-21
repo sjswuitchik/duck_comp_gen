e4fcc2b58f3dac4765db0ca740b6eabd3b7c5a72
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-21 12:29:45 -0400

    Update run_angsd.sh

--- NCBI_run/05_CompPopGen/03_popGen/run_angsd.sh
+++ NCBI_run/05_CompPopGen/03_popGen/run_angsd.sh
@@ -5,6 +5,6 @@
 #SBATCH --time=24:00:00
 source activate vcfqc
 
-angsd -b bams -GL 2 -out hetAtr -doMajorMinor -doMaf -remove_bads 1 -checkBamHeaders 0
+angsd -b bams -GL 2 -out hetAtr -doMajorMinor 1 -doMaf 1
 angsd -b bams -GL 2 -doSaf 1 -doMajorMinor 1 -doMaf 1 -anc hetAtr.ncbi.fasta -ref hetAtr.ncbi.fasta -out hetAtr
 realSFS hetAtr.saf.idx -fold 1 -P ${SLURM_JOB_CPUS_PER_NODE} > hetAtr.sfs
