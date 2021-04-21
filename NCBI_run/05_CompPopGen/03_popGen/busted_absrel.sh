6e95ff186833f87a7174adbb56f8117bca229a19
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-21 12:48:10 -0400

    Update busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
@@ -1,4 +1,4 @@
-# in /n/holyscratch01/informatics/swuitchik/ducks/compGen
+# in /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted
 
 source activate vcfqc
 
@@ -6,5 +6,12 @@ source activate vcfqc
 wget http://wasabiapp.org/download/prank/prank.linux64.170427.tgz
 tar zxvf http://wasabiapp.org/download/prank/prank.linux64.170427.tgz
 rm prank.linux64.170427.tgz 
+
 # grab gallo HAL from WGA
 cp /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/02_wga/gallo_ncbi.hal .
+# convert to MAF
+singularity shell --cleanenv /n/singularity_images/informatics/cat/cat:20200604.sif
+hal2maf gallo_ncbi.hal gallo_ncbi.maf --refGenome galGal --noAncestors --noDupes
+mafTools/bin/mafValidator.py --maf=gallo_ncbi.maf
+
+
