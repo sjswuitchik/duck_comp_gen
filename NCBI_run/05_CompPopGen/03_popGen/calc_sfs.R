a56b4687cb0356134b057adb8491aaf786588fe0
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-22 16:55:26 -0400

    Create calc_sfs.R

---
+++ NCBI_run/05_CompPopGen/03_popGen/calc_sfs.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+#read in data, rename col names
+data <- read_delim("~/Desktop/hetAtr.frq.count", delim = '\t', col_names = T) %>%
+  rename(chrom = CHROM, pos = POS, n_alleles = N_ALLELES, n_chr = N_CHR, major = MAJOR, AC = MINOR) 
+
+# calc folded SFS
+fold <- data %>%
+  mutate(fold = ifelse(AC > 15, 30 - AC, AC)) %>%
+  filter(fold > 0)
+# put SFS into its own var
+sfs <- table(fold$fold)
+# check SFS
+pdf("~/Desktop/sfs.pdf")
+plot(sfs)
+dev.off()
+# write out so you can manually put into the Stairway blueprint file 
+sfs %>% 
+  as.data.frame() %>%
+  write_delim(., "~/Desktop/sfs.tsv", delim = '\t', col_names = T)
