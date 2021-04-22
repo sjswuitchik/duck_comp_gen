9a80010a22e5136ac0174c04ac024f89309c83fe
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-22 16:55:16 -0400

    Update stairway.sh

--- NCBI_run/05_CompPopGen/03_popGen/stairway.sh
+++ NCBI_run/05_CompPopGen/03_popGen/stairway.sh
@@ -11,7 +11,8 @@ mv stairway-plot-v2/ stairway/
 cd stairway
 cp ../hetAtr_stiNae_qc/hetAtr.filtered.vcf .
 vcftools --vcf hetAtr.filtered.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --remove-indels --out hetAtr --counts2
-
+# edit frq.counts header to be CHROM  POS N_ALLELES N_CHR MAJOR MINOR
+Rscript calc_sfs.R
 
 # run Stairbuilder
 mkdir plots
