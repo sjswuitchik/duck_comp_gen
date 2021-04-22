a6984a418bf9934ac50dc7ce4350b973bff18165
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-22 10:22:58 -0400

    Update busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
@@ -1,7 +1,7 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted
 
-source activate vcfqc
-#conda install -c conda-forge libgcc-ng
+#conda create -n busted -c bioconda prank hyphy
+source activate busted
 
 # download PRANK
 wget http://wasabiapp.org/download/prank/prank.linux64.170427.tgz
