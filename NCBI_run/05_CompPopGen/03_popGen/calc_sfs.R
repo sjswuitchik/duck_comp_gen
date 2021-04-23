dfafc5870a69c2c4f2f3db13c43cd438a25a2d9c
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-23 09:13:26 -0400

    Update calc_sfs.R

--- NCBI_run/05_CompPopGen/03_popGen/calc_sfs.R
+++ NCBI_run/05_CompPopGen/03_popGen/calc_sfs.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 #read in data, rename col names
-data <- read_delim("~/Desktop/hetAtr.frq.count", delim = '\t', col_names = T) %>%
+data <- read_delim("hetAtr.frq.count", delim = '\t', col_names = T) %>%
   rename(chrom = CHROM, pos = POS, n_alleles = N_ALLELES, n_chr = N_CHR, major = MAJOR, AC = MINOR) 
 
 # calc folded SFS
@@ -11,10 +11,10 @@ fold <- data %>%
 # put SFS into its own var
 sfs <- table(fold$fold)
 # check SFS
-pdf("~/Desktop/sfs.pdf")
+pdf("sfs.pdf")
 plot(sfs)
 dev.off()
 # write out so you can manually put into the Stairway blueprint file 
 sfs %>% 
   as.data.frame() %>%
-  write_delim(., "~/Desktop/sfs.tsv", delim = '\t', col_names = T)
+  write_delim(., "sfs.tsv", delim = '\t', col_names = T)
