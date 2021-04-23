2b01c4e48ff1a3cb494e5f0ae722ced74458829c
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-23 11:05:47 -0400

    Update busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
@@ -5,13 +5,11 @@ source activate busted
 
 # bring over protein FASTAs and SeqIDs from OrthoFinder
 mkdir fastas
-cp -v /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/04_OrthoFinder/run_ortho/Results_Feb01/WorkingDirectory/Species*.fa SequenceIDs.txt fastas/
-
-
-# download PRANK
-wget http://wasabiapp.org/download/prank/prank.linux64.170427.tgz
-tar zxvf prank.linux64.170427.tgz
-rm prank.linux64.170427.tgz 
+cp -v /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/04_OrthoFinder/run_ortho/Results_Feb01/WorkingDirectory/Species*.fa /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/04_OrthoFinder/run_ortho/Results_Feb01/WorkingDirectory/SequenceIDs.txt fastas/
+for file in fastas/*.fa;
+do
+  awk 'NR == FNR {seqid[">"substr($1, 1, length($1)-1)] = $2; next} /^>/ { print ">" seqid[$1]; next} {print}' fastas/SequenceIDs.txt $file > $file.named
+done
 
 
 
