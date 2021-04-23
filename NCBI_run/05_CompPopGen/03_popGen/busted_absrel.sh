4611fa49ad1c4bc2631a8a2346e66f80dccf98a7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-23 12:00:38 -0400

    Update busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
@@ -6,10 +6,19 @@ source activate busted
 # bring over protein FASTAs and SeqIDs from OrthoFinder
 mkdir fastas
 cp -v /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/04_OrthoFinder/run_ortho/Results_Feb01/WorkingDirectory/Species*.fa /n/holylfs/LABS/informatics/swuitchik/ducks/ncbi_analyses/04_OrthoFinder/run_ortho/Results_Feb01/WorkingDirectory/SequenceIDs.txt fastas/
+
+# translate the def lines from OrthoFinder to actual seq IDs
 for file in fastas/*.fa;
 do
   awk 'NR == FNR {seqid[">"substr($1, 1, length($1)-1)] = $2; next} /^>/ { print ">" seqid[$1]; next} {print}' fastas/SequenceIDs.txt $file > $file.named
 done
 
+# rename the species FASTAs to the actual species codes
+while read id file; 
+do
+  mv Species${id%:}.fa.named ${file%%.translated.fa}.fa;
+done < SpeciesIDs.txt
 
-
+# tidy
+mkdir orig_fastas
+mv Species*.fa orig_fastas
