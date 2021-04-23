dabb1f5a27753b9bc5c17d29d31fdbbdd20b8de5
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-23 10:19:00 -0400

    Update busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel.sh
@@ -1,7 +1,10 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted
 
-#conda create -n busted -c bioconda prank hyphy
+#conda create -n busted -c bioconda prank hyphy emboss
 source activate busted
+
+# bring over protein alignments from OrthoFinder
+
 
 # download PRANK
 wget http://wasabiapp.org/download/prank/prank.linux64.170427.tgz
