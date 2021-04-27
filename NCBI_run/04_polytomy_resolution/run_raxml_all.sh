6f3a383cbd5df6860d1d9b8e64aca4fa4ad78200
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-27 11:57:27 -0400

    Create run_raxml_all.sh

---
+++ NCBI_run/04_polytomy_resolution/run_raxml_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J RAxML
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p shared
+#SBATCH -n 8
+#SBATCH -t 06-23:00:00
+#SBATCH --mem=12000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee/trimmed/finished_fastas/
+
+source activate raxml
+
+for file in *.fa;
+do
+  raxml-ng --msa $file --model HKY+G4 --prefix $file --all --seed 2 --redo
+done
+
+cat *.bestTree > final.all.tree
