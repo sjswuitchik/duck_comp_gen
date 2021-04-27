de869cace1f56a7328789c6845df029ea854bb2a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-27 11:50:13 -0400

    Update 03_align_raxml.sh

--- NCBI_run/04_polytomy_resolution/03_align_raxml.sh
+++ NCBI_run/04_polytomy_resolution/03_align_raxml.sh
@@ -38,5 +38,5 @@ sbatch run_raxml.sh
 # test --all against simple inference in /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee/trimmed/finished_fastas
 for file in *.fa;
 do
-  raxml-ng --msa $file --model HKY+G4 --all --prefix $file --seed 2
+  raxml-ng --msa $file --model HKY+G4 --all --prefix $file --seed 2 --redo > log 2> err
 done
