54bf8aa644590da8bae4c5cb09c2f68117bde997
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-04-30 17:41:03 -0400

    Create run_prank.sh

---
+++ NCBI_run/05_CompPopGen/03_popGen/run_prank.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J prank
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 06-23:00:00
+#SBATCH --mem=12000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/align/
+
+source activate busted
+
+for file in *.dna;
+do
+  prank -d=$file -o=$file -f=fasta -support -DNA
+done
