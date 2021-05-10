8af1f94da83f46e129e6fd2cb02970dc1111ea73
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-05-10 11:56:51 -0400

    Update 05_densitree_prep.sh

--- NCBI_run/04_polytomy_resolution/05_densitree_prep.sh
+++ NCBI_run/04_polytomy_resolution/05_densitree_prep.sh
@@ -6,6 +6,8 @@ do
   cp $file densitree_input/
 done
 
+cp final.all.tree densitree_input/
+
 cd densitree_input/
 
 for file in *.bestTree;
@@ -13,3 +15,4 @@ do
   python3 newick2nexus.py -i $file -o $file.nex
 done
 
+python3 newick2nexus.py -i final.all.tree -o final.all.tree.nex
