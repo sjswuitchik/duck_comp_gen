fb36bb85aad18c18e4b46210e47b83483321610c
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-05-19 10:40:54 -0400

    Update run_prank.sh

--- NCBI_run/05_CompPopGen/03_popGen/run_prank.sh
+++ NCBI_run/05_CompPopGen/03_popGen/run_prank.sh
@@ -7,11 +7,11 @@
 #SBATCH -t 06-23:00:00
 #SBATCH --mem=12000
 
-# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/unaligned
 
 source activate busted
 
-for file in unaligned/*.fa;
+for file in *.fa;
 do
   prank -d=$file -o=$file -f=fasta -support -DNA
 done
