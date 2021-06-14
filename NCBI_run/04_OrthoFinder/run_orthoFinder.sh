dd928400a398a17fa813bc8cf61b946eb9111982
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-06-14 11:12:21 -0400

    Update run_orthoFinder.sh

--- NCBI_run/04_OrthoFinder/run_orthoFinder.sh
+++ NCBI_run/04_OrthoFinder/run_orthoFinder.sh
@@ -7,7 +7,5 @@
 #SBATCH -e ortho-%j.err
 #SBATCH -o ortho-%j.out
 
-module purge
-module load Anaconda3/2019.10
 source activate ortho
 orthofinder -o /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/04a_OrthoFinder_compAug/run_ortho/OrthoFinder -f run_ortho/input_data
