43e362adf602bb0eb43f6c9493b5a3fa4bb0a4a1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-06-14 11:12:48 -0400

    Update 01_align_cnees_run_mafft.sh

--- NCBI_run/03a_cnee_analysis/01_align_cnees_run_mafft.sh
+++ NCBI_run/03a_cnee_analysis/01_align_cnees_run_mafft.sh
@@ -2,8 +2,6 @@
 ## CNEE alignment - NCBI ##
 ###########################
 
-module load Anaconda3/2019.10
-
 # in /n/holyscratch01/informatics/swuitchik/ducks_project/ncbi_run/03a_cnee_analysis
 
 mkdir -p alignments
