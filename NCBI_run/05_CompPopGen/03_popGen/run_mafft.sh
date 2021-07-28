f84fec577e449f215212fc30021a0bb360e206d1
sjswuitchik <sjswuit@g.harvard.edu>
2021-07-28 14:25:30 -0400

    Creating run_mafft.sh

---
+++ NCBI_run/05_CompPopGen/03_popGen/run_mafft.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J mafft
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 06-23:00:00
+#SBATCH --mem=15000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/unaligned
+
+source activate align
+
+for file in *.fa;
+do
+mafft --quiet $file > $file.mafft
+done
