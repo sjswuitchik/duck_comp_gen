b964ecded6bbf9b3233a6014d0403f12cd46595a
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-08-06 10:07:50 -0400

    Create run_hmmcleaner.sh

---
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel/run_hmmcleaner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J hmmCl
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 06-23:00:00
+#SBATCH --mem=15000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/aligned
+
