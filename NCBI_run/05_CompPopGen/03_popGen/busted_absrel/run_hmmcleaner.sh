121d24173d73d73068d07761d2f07e20b874c47b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-08-06 10:08:31 -0400

    Update run_hmmcleaner.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel/run_hmmcleaner.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel/run_hmmcleaner.sh
@@ -9,3 +9,7 @@
 
 # submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted/aligned
 
+for file in *.mafft;
+do
+  singularity exec --cleanenv /n/singularity_images/informatics/hmmcleaner/hmmcleaner_0.180750.sif HmmCleaner.pl $file
+done
