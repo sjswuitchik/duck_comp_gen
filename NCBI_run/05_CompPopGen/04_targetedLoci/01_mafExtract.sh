18e77222fc608190897e6c2f6130ffc8842408fa
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-10-05 11:26:21 -0700

    Update 01_mafExtract.sh

--- NCBI_run/05_CompPopGen/04_targetedLoci/01_mafExtract.sh
+++ NCBI_run/05_CompPopGen/04_targetedLoci/01_mafExtract.sh
@@ -4,4 +4,5 @@
 singularity shell --cleanenv /n/singularity_images/informatics/cat/cat:20200604.sif
 hal2maf gallo_ncbi.hal gallo_ncbi.maf --refGenome galGal --noAncestors --noDupes
 
-singularity exec --cleanenv /n/singularity_images/informatics/maftools/maftools:20170913.sif mafExtractor --maf gallo_ncbi.maf --seq NC_006127.5 --start 65334667 --stop 65337079
+singularity exec --cleanenv /n/singularity_images/informatics/maftools/maftools:20170913.sif mafExtractor --maf gallo_ncbi.maf --seq galGal.NC_006127.5 --start 65334667 --stop 65337079 > test.maf
+
