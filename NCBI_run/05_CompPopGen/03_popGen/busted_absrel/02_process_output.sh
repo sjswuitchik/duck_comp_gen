736891df82c92b6d950f6440043ff68bd137bdad
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-10-25 15:27:16 -0700

    Create 02_process_output.sh

---
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel/02_process_output.sh
@@ -0,0 +1,11 @@
+# in /n/holyscratch01/informatics/swuitchik/ducks/compGen
+
+mkdir busted_out
+mkdir absrel_out
+cp busted/aligned/mafft/clean_align/all_spp/*BUSTED* busted_out
+cp busted/aligned/mafft/last2hmmClean/*BUSTED* busted_out
+cp busted/aligned/mafft/clean_align/all_spp/*ABSREL* absrel_out
+cp busted/aligned/mafft/last2hmmClean/*ABSREL* absrel_out
+
+git clone https://github.com/gwct/core.git
+python3 core/hyphy-interface/hyphy_to_csv.py -i busted_out -o
