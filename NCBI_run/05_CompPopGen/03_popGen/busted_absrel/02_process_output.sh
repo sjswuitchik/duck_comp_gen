f30d1070f728b12a7809682f566ff04043520b90
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-10-26 11:18:03 -0700

    Update 02_process_output.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel/02_process_output.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel/02_process_output.sh
@@ -8,5 +8,5 @@ cp busted/aligned/mafft/clean_align/all_spp/*ABSREL* absrel_out
 cp busted/aligned/mafft/last2hmmClean/*ABSREL* absrel_out
 
 git clone https://github.com/gwct/core.git
-python3 core/hyphy-interface/hyphy_to_csv.py -i busted_out -m busted --overwrite -o busted_output
-python3 core/hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o absrel_output
+python3 core/hyphy-interface/hyphy_to_csv.py -i busted_out -m busted --overwrite -o busted_output.csv
+python3 core/hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o absrel_output.csv
