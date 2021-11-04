86ca42934455b4a4816af70747f77a4095806d79
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-04 05:35:36 -0700

    Update 01_busted_absrel.sh

--- NCBI_run/05_CompPopGen/03_popGen/busted_absrel/01_busted_absrel.sh
+++ NCBI_run/05_CompPopGen/03_popGen/busted_absrel/01_busted_absrel.sh
@@ -1,6 +1,6 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/compGen/busted
 
-#conda create -n busted -c bioconda hyphy bedtools r-base r-tidyverse
+#conda create -n busted -c bioconda hyphy bedtools r-base r-tidyverse mafft
 source activate busted
 
 mkdir fastas
