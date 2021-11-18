76a3fcabaad8723ae1b3d1315c22bf9c4fa90321
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-18 11:34:06 -0800

    Create clean_ogs.R

---
+++ NCBI_run/04_OrthoFinder/clean_ogs.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+data <- read_delim("Orthogroups.GeneCount.tsv", delim = '\t')
+
+data %>%
+  filter(Total <= 25) %>%
+  filter(hetAtr.translated != 0) %>%
+  rowwise() %>%
+  mutate(missing_spp = sum(c_across(anaPla_protein:tymCupPin.translated) == 0)) %>%
+  filter(missing_spp <= 7) %>%
+  write_delim(., "clean_ogs_geneCount.tsv", delim = '\t')
