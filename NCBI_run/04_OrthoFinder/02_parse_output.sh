01b43e19e9543cd0ed8ae60ddc62bb79c070c253
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-18 11:36:22 -0800

    Update 02_parse_output.sh

--- NCBI_run/04_OrthoFinder/02_parse_output.sh
+++ NCBI_run/04_OrthoFinder/02_parse_output.sh
@@ -1,6 +1,6 @@
-# in /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Orthogroup_Sequences
+# in /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/
 
-for file in *.fa;
+for file in results/Results_Nov09/Orthogroup_Sequences/*.fa;
 do
   export n=$(grep -c '^>' $file)
   echo $file'_'$n >> seq_counts.txt
