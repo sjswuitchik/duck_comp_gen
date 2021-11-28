298d3f8d9d87003d3db309970ed5333306bc79a0
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-27 21:32:09 -0800

    Update 02_parse_output.sh

--- 04_OrthoFinder/02_parse_output.sh
+++ 04_OrthoFinder/02_parse_output.sh
@@ -6,12 +6,39 @@ do
   echo $file'_'$n >> seq_counts.txt
 done
 
+# get OGs that are in hetAtr, in at least 50% of spp, and capped at 25 seq
 Rscript clean_ogs.R
 
+# get protein IDs for each species on NCBI
 cd /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/ncbi_data/
-for file in anaPla ansCyg colVir cotJap galGal numMel
+for file in anaPla ansCyg colVir cotJap galGal numMel;
 do
   grep -o 'protein_id=.._[0-9]*.[0-9]' $file/${file}_cds_from_genomic.fna > ${file}_protID.tsv
 done
+
+# check numbers match
+for file in anaPla ansCyg colVir cotJap galGal numMel;
+do
+  grep -c 'protein_id=' $file/${file}_cds_from_genomic.fna
+  wc -l ${file}_protID.tsv
+done
+
+#39905
+#39905 anaPla_protID.tsv
+#31811
+#31811 ansCyg_protID.tsv
+#17165
+#0 colVir_protID.tsv
+#40894
+#40894 cotJap_protID.tsv
+#49681
+#49681 galGal_protID.tsv
+#43240
+#43240 numMel_protID.tsv
+
+# didn't work for colVir, check format of prot ID for that spp
+grep -o 'protein_id=OXB[0-9]*.[0-9]' colVir/colVir_cds_from_genomic.fna > colVir_protID.tsv
+wc -l colVir_protID.tsv 
+# 17165 colVir_protID.tsv
 
 Rscript clean_prots.R
