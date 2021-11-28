4d00edfea4a6df0a3d736de270bf4df4597927c0
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-28 14:07:16 -0800

    Update 02_parse_output.sh

--- 04_OrthoFinder/02_parse_output.sh
+++ 04_OrthoFinder/02_parse_output.sh
@@ -43,7 +43,7 @@ wc -l colVir_protID.tsv
 
 for spp in anaPla ansCyg colVir cotJap galGal numMel;
 do
-  Rscript ncbi_data/clean_prots.R ${spp}_protID.tsv ${spp}_cleanID.tsv
+  Rscript clean_prots.R ncbi_data/${spp}_protID.tsv ${spp}_cleanID.tsv
 done
 
 
