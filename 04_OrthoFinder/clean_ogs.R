82b11d735a4bb0cd56b71a7e4ca803fa7f67f0bb
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-28 13:50:41 -0800

    Update clean_ogs.R

--- 04_OrthoFinder/clean_ogs.R
+++ 04_OrthoFinder/clean_ogs.R
@@ -2,11 +2,18 @@ library(tidyverse)
 
 data <- read_delim("run_ortho/results/Results_Nov09/Orthogroups/Orthogroups.GeneCount.tsv", delim = '\t')
 
-data %>%
+clean <- data %>%
   filter(Total <= 25) %>%
   filter(hetAtr.translated != 0) %>%
   rowwise() %>%
   mutate(missing_spp = sum(c_across(anaPla_protein:tymCupPin.translated) == 0)) %>%
   filter(missing_spp <= 7) %>%
-  select(Orthogroup) %>%
-  write_delim(., "clean_ogs_geneCount.tsv", delim = '\t')
+  select(Orthogroup)
+
+write_delim(clean, "clean_ogs_geneCount.tsv", delim = '\t')
+
+orthos <- read_delim("run_ortho/results/Results_Nov09/Orthogroups/Orthogroups.tsv", delim = '\t')
+
+clean_orthos <- left_join(clean, orthos, by = "Orthogroup")
+
+write_delim(clean_orthos, "clean_orthos.tsv", delim = '\t')
