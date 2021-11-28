ae4a2798320235781a995ef96855e37edfe1c4de
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-28 14:00:15 -0800

    Update clean_prots.R

--- 04_OrthoFinder/clean_prots.R
+++ 04_OrthoFinder/clean_prots.R
@@ -1,1 +1,14 @@
+#!/usr/bin/Rscript
+
 library(tidyverse)
+
+args <- commandArgs(trailingOnly = T)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).\n", call.=FALSE)
+} 
+
+spp <- read_delim(args[1], delim = '\t', col_names = F) %>%
+  separate(X1, into = c(NA, "X1"), sep = '=') %>%
+  write_delim(., args[2], delim = '\t')
