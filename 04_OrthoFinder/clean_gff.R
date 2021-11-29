fa8ff88acb0d235d7b8090bfd2794462015fa944
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-11-28 17:31:58 -0800

    Create clean_gff.R

---
+++ 04_OrthoFinder/clean_gff.R
@@ -0,0 +1,16 @@
+#!/usr/bin/Rscript
+
+library(tidyverse)
+
+args <- commandArgs(trailingOnly = T)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).\n", call.=FALSE)
+} 
+
+read_delim(args[1], col_names = F) %>%
+  select(X9) %>%
+  separate(X9, into = c("trans", NA), sep = ';') %>%
+  separate(trans, into = c(NA, "id"), sep = ' ') %>%
+  write_delim(args[2], col_names = F)
