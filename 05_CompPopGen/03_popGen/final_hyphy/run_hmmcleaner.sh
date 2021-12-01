c32452c13a6d78544b2306cfcc7601afa14054c9
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-01 11:07:33 -0800

    Create run_hmmcleaner.sh

---
+++ 05_CompPopGen/03_popGen/final_hyphy/run_hmmcleaner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J hmmCl
+#SBATCH -o out_hm
+#SBATCH -e err_hm
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 06-23:00:00
+#SBATCH --mem=15000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/final_hyphy/aligned
+
+for file in *.mafft;
+do
+if [ -f ${file}_hmm.fasta]; then
+  continue;
+fi
+  singularity exec --cleanenv /n/singularity_images/informatics/hmmcleaner/hmmcleaner_0.180750.sif HmmCleaner.pl $file
+done
