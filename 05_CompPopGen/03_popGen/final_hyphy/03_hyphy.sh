f2f92876742ce44caa2397ae504033071f6ff268
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-08 11:33:08 -0800

    Create 03_hyphy.sh

---
+++ 05_CompPopGen/03_popGen/final_hyphy/03_hyphy.sh
@@ -0,0 +1,20 @@
+# in /n/holyscratch01/informatics/swuitchik/ducks/compGen
+
+# copy over gene trees
+mkdir gene_trees
+cut -f1 /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/clean_orthos.tsv | sed -e "1d" > clean_ogs.tsv
+
+while IFS= read -r file
+do
+  cp -vr /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Gene_Trees/${file}_tree.txt gene_trees/
+done < "clean_ogs.tsv"
+
+cd gene_trees
+for file in *.txt;
+do
+  sed -i 's/\_protein//g' $file 
+  sed -i 's/\_translated//g' $file
+done
+
+
+
