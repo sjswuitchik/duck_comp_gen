7817fa2674eff63aec72dd9caa8d10a14f6ce879
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-08 16:48:47 -0800

    Create singleCopy_clean_ogs.R

---
+++ 05_CompPopGen/03_popGen/final_hyphy/subscripts/singleCopy_clean_ogs.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+
+single_orthos <- read_delim("Orthogroups_SingleCopyOrthologues.txt", delim = '\t', col_names = c("Orthogroup"))
+all_orthos <- read_delim("Orthogroups.tsv", delim = '\t') %>%
+  select(-c(...2))
+
+clean_orthos <- left_join(single_orthos, all_orthos, by = "Orthogroup")
+
+clean_orthos %>%
+  select(Orthogroup, anaPla = anaPla_protein) %>%
+  write_delim(., "anaPla_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, ansBra = ansBra.translated) %>%
+  write_delim(., "ansBra_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, ansCyg = ansCyg_protein) %>%
+  write_delim(., "ansCyg_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, ansInd = ansInd.translated) %>%
+  write_delim(., "ansInd_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, braCan = braCan.translated) %>%
+  write_delim(., "braCan_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, colVir = colVir_protein) %>%
+  write_delim(., "colVir_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, cotJap = cotJap_protein) %>%
+  write_delim(., "cotJap_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, galGal = galGal_protein) %>%
+  write_delim(., "galGal_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, hetAtr = hetAtr.translated) %>%
+  write_delim(., "hetAtr_ogs.tsv", delim = '\t')
+
+clean_orthos %>%
+  select(Orthogroup, netAur = netAur.translated) %>%
+  write_delim(., "netAur_ogs.tsv", delim = '\t')
+
+ clean_orthos %>%
+  select(Orthogroup, numMel = numMel_protein) %>%
+  write_delim(., "numMel_ogs.tsv", delim = '\t')
+ 
+ clean_orthos %>%
+  select(Orthogroup, oxyJam = oxyJam.translated) %>%
+  write_delim(., "oxyJam_ogs.tsv", delim = '\t')
+ 
+clean_orthos %>%
+  select(Orthogroup, stiNae = stiNae.translated) %>%
+  write_delim(., "stiNae_ogs.tsv", delim = '\t') 
+ 
+clean_orthos %>%
+  select(Orthogroup, syrMik = syrMik.translated) %>%
+  write_delim(., "syrMik_ogs.tsv", delim = '\t') 
+ 
+clean_orthos %>%
+  select(Orthogroup, tymCupPin = tymCupPin.translated) %>%
+  write_delim(., "tymCupPin_ogs.tsv", delim = '\t')
+ 
+
+write_delim(clean_orthos, "clean_single_orthos.tsv", delim = '\t')
