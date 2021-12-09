e7d395b3f5f8966ff965ba106ff7da4d4329ffe4
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-08 16:41:26 -0800

    Update 04_singleCopy_OGs.sh

--- 05_CompPopGen/03_popGen/final_hyphy/04_singleCopy_OGs.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/04_singleCopy_OGs.sh
@@ -4,3 +4,5 @@
 mkdir -p single_copies/fastas
 cp -vr /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Single_Copy_Orthologue_Sequences/*.fa single_copies/fastas
 cp -vr /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Orthogroups/Orthogroups_SingleCopyOrthologues.txt single_copies
+cp -vr /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Orthogroups/Orthogroups.tsv single_copies
+
