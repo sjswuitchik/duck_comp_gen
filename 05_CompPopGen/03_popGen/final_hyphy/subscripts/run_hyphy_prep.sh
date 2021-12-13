411643e6fbe0cca24c4ffb198aee1f5ead8e4950
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-13 13:51:12 -0800

    Create run_hyphy_prep.sh

---
+++ 05_CompPopGen/03_popGen/final_hyphy/subscripts/run_hyphy_prep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J hyphyPrep
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 03-00:00
+#SBATCH --mem=15000
+
+# submit from /n/holyscratch01/informatics/swuitchik/ducks/compGen/og_fastas
+
+conda activate align
+
+for file in *.fa;
+do
+  hyphy hyphy-analyses/codon-msa/pre-msa.bf --input $file
+  muscle -in ${file}_protein.fas -out ${file}_protein.msa
+  hyphy hyphy-analyses/codon-msa/post-msa.bf --protein-msa ${file}_protein.msa --nucleotide-sequences ${file}_nuc.fas --output ${file}_codon.msa --compress No
+done
