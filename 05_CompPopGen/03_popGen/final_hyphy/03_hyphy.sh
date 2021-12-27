f9983fddf36fbcaca18a76ee0ea75b025a0e68fd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-27 12:05:16 -0800

    Update 03_hyphy.sh

--- 05_CompPopGen/03_popGen/final_hyphy/03_hyphy.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/03_hyphy.sh
@@ -1,34 +1,5 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/compGen
 
-# copy over gene trees
-mkdir gene_trees
-cut -f1 /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/clean_orthos.tsv | sed -e "1d" > clean_ogs.tsv
-
-while IFS= read -r file
-do
-  cp -vr /n/holyscratch01/informatics/swuitchik/ducks/orthofinder_nov2021/run_ortho/results/Results_Nov09/Gene_Trees/${file}_tree.txt gene_trees/
-done < "clean_ogs.tsv"
-
-# edit gene trees to match alignments
-cd gene_trees
-for file in *.txt;
-do
-  sed -i 's/\_protein//g' $file 
-  sed -i 's/\_translated//g' $file
-done
-
-# remove duplicate sequences and produce combined alignment + tree for HYPHY input
-cd ..
-mkdir clean_aligned
-sbatch run_removeDups.sh
-
-# create list of OGs that were dedup'd
-cd clean_aligned 
-ls *.nh > uniq
-sed -i 's/\_uniq\.nh//g' uniq
-mv uniq ..
-Rscript split_dups.R
-
 ## make BUSTED job scripts
 mkdir -p job_scripts_busted/logs
 
