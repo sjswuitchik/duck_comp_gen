8aa7004e87ec6c66e08d3c327162b8a92d696237
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-01-11 09:50:28 -0800

    Update 04_process_output.sh

--- 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
@@ -5,6 +5,6 @@ mkdir absrel_out
 cp -v og_fastas/*BUSTED.json busted_out
 cp -v og_fastas/*ABSREL.json absrel_out
 
-git clone https://github.com/gwct/core.git
-python3 core/hyphy-interface/hyphy_to_csv.py -i busted_out -m busted --overwrite -o busted_output.csv
-python3 core/hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o absrel_output.csv
+git clone https://github.com/gwct/hyphy-interface.git
+python3 hyphy-interface/hyphy_to_csv.py -i busted_out -m busted --overwrite -o busted_output.csv
+python3 hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o absrel_output.csv
