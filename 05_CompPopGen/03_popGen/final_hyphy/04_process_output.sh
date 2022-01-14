91d5c042a21aef87c2a8dcf0ce0d25ff8f0c184d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-01-14 12:59:28 -0800

    Update 04_process_output.sh

--- 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
@@ -13,3 +13,7 @@ python3 hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o a
 # clean up output (keep run meta data but write out file without header for easier analysis)
 grep -v '^#' busted_output.csv > busted_output_clean.csv
 grep -v '^#' absrel_output.csv > absrel_output_clean.csv
+
+# filter aBSREL results for OGs with hetAtr 
+grep 'hetAtr' absrel_output_clean.csv > absrel_output_clean_hetAtr.csv
+sed -i '1s/^/file\,branches\,num\_ps\_branches\,ps\_pvals\n/' absrel_output_clean_hetAtr.csv
