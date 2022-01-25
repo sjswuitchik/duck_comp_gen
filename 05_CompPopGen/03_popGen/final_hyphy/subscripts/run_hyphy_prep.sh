86ad3fe52f7948cb42a739b328acc06871eb2bc6
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-01-25 11:11:36 -0800

    Update run_hyphy_prep.sh

--- 05_CompPopGen/03_popGen/final_hyphy/subscripts/run_hyphy_prep.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/subscripts/run_hyphy_prep.sh
@@ -14,6 +14,6 @@ conda activate align
 for file in *.fa;
 do
   hyphy hyphy-analyses/codon-msa/pre-msa.bf --input $file
-  mafft --quiet ${file}_protein.fas > ${file}_protein.msa
+  mafft ${file}_protein.fas > ${file}_protein.msa
   hyphy hyphy-analyses/codon-msa/post-msa.bf --protein-msa ${file}_protein.msa --nucleotide-sequences ${file}_nuc.fas --output ${file}_codon.msa --compress No
 done
