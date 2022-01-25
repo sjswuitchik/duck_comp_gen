073bfeac6d0b302c03c9e74f4c407e8f7039d2c1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-01-25 11:39:12 -0800

    Delete split_dups.R

--- 05_CompPopGen/03_popGen/final_hyphy/subscripts/split_dups.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-clean <- read_delim("clean_ogs.tsv", delim = '\t', col_names = c("Orthogroup"))
-uniq <- read_delim("uniq", delim = '\t', col_names = c("Orthogroup"))
-
-anti_join(clean, uniq, by = "Orthogroup") %>%
-  write_delim(., "split", delim = '\t', col_names = F)
