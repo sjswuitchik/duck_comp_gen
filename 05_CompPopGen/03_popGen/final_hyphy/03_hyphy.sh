1e70072e3eae0afb3b98fa9ac918b7b2c016d6e1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-02-02 14:26:56 -0800

    Update 03_hyphy.sh

--- 05_CompPopGen/03_popGen/final_hyphy/03_hyphy.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/03_hyphy.sh
@@ -1,9 +1,8 @@
 # in /n/holyscratch01/informatics/swuitchik/ducks/compGen/og_fastas
 
-## make BUSTED job scripts
+# make BUSTED job scripts
 mkdir logs
 
-# make scripts 
 while IFS= read -r file
 do
   echo -e '#!/bin/bash' >> run_busted_${file}.sh
@@ -16,20 +15,6 @@ do
   echo -e "source activate align\n" >> run_busted_${file}.sh
   echo -e "hyphy busted --alignment ${file}_codon_hmm.fasta --tree ${file}_prunedTree.txt" >> run_busted_${file}.sh
 done < "clean_aligns"
-
-# make scripts for runs with duplicate seqs 
-#while IFS= read -r file
-#do
-#  echo -e '#!/bin/bash' >> run_busted_${file}.sh
-#  echo -e "#SBATCH -o logs/${file}_busted.out" >> run_busted_${file}.sh
-#  echo -e "#SBATCH -e logs/${file}_busted.err" >> run_busted_${file}.sh
-#  echo -e "#SBATCH -p shared" >> run_busted_${file}.sh
-#  echo -e "#SBATCH -n 1" >> run_busted_${file}.sh
-#  echo -e "#SBATCH -t 48:00:00" >> run_busted_${file}.sh
-#  echo -e "#SBATCH --mem=9000\n" >> run_busted_${file}.sh
-#  echo -e "source activate align\n" >> run_busted_${file}.sh
-#  echo -e "hyphy busted --alignment ${file}_uniq.fas --tree ${file}_tree.txt" >> run_busted_${file}.sh
-#done < "uniq"
 
 # create BUSTED batches
 ls *busted*.sh > busted_scripts
@@ -50,9 +35,7 @@ do
   sbatch $file
 done < "busted_batch03"
 
-## make aBSREL job scripts
-
-# make scripts for runs without duplicate seqs
+# make aBSREL job scripts
 while IFS= read -r file
 do
   echo -e '#!/bin/bash' >> run_absrel_${file}.sh
@@ -63,22 +46,8 @@ do
   echo -e "#SBATCH -t 48:00:00" >> run_absrel_${file}.sh
   echo -e "#SBATCH --mem=9000\n" >> run_absrel_${file}.sh
   echo -e "source activate align\n" >> run_absrel_${file}.sh
-  echo -e "hyphy absrel --alignment ${file}_nuc.fa_codon.msa --tree ${file}_prunedTree.txt" >> run_absrel_${file}.sh
-done < "split_aligns"
-
-# make scripts for runs with duplicate seqs 
-while IFS= read -r file
-do
-  echo -e '#!/bin/bash' >> run_absrel_${file}.sh
-  echo -e "#SBATCH -o logs/${file}_absrel.out" >> run_absrel_${file}.sh
-  echo -e "#SBATCH -e logs/${file}_absrel.err" >> run_absrel_${file}.sh
-  echo -e "#SBATCH -p shared" >> run_absrel_${file}.sh
-  echo -e "#SBATCH -n 1" >> run_absrel_${file}.sh
-  echo -e "#SBATCH -t 48:00:00" >> run_absrel_${file}.sh
-  echo -e "#SBATCH --mem=9000\n" >> run_absrel_${file}.sh
-  echo -e "source activate align\n" >> run_absrel_${file}.sh
-  echo -e "hyphy absrel --alignment ${file}_uniq.fas --tree ${file}_tree.txt" >> run_absrel_${file}.sh
-done < "uniq"
+  echo -e "hyphy absrel --alignment ${file}_codon_hmm.fasta --tree ${file}_prunedTree.txt" >> run_absrel_${file}.sh
+done < "clean_aligns"
 
 # create aBSREL batches
 ls *absrel*.sh > absrel_scripts
