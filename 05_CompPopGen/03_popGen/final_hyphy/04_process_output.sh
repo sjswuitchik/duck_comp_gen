9fa94fd16c4f0f2cd0a0806e361d4d8d804bd4c4
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-03-02 11:00:37 -0800

    Update 04_process_output.sh

--- 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/04_process_output.sh
@@ -11,8 +11,8 @@ python3 hyphy-interface/hyphy_to_csv.py -i busted_out -m busted --overwrite -o b
 python3 hyphy-interface/hyphy_to_csv.py -i absrel_out -m absrel --overwrite -o absrel_output.csv
 
 # clean up output (keep run meta data but write out file without header for easier analysis)
-grep -v '^#' busted_output.csv | sed 's/\_nuc\.fa\_codon\.msa\.BUSTED\.json//g' | sed 's/\_uniq\.fas\.BUSTED\.json//g' > busted_output_clean.csv
-grep -v '^#' absrel_output.csv | sed 's/\_nuc\.fa\_codon\.msa\.ABSREL\.json//g' | sed 's/\_uniq\.fas\.ABSREL\.json//g' > absrel_output_clean.csv
+grep -v '^#' busted_output.csv | sed 's/\_codon\_hmm\.fasta\.BUSTED\.json//g' | sed 's/\_uniq\_hmm\.fasta\.BUSTED\.json//g' > busted_output_clean.csv
+grep -v '^#' absrel_output.csv | sed 's/\_codon\_hmm\.fasta\.ABSREL\.json//g' | sed 's/\_uniq\_hmm\.fasta\ABSREL\.json//g' > absrel_output_clean.csv
 
 # filter aBSREL results for OGs with hetAtr 
 awk -F , '$2 ~ /hetAtr/ { nf = split($2, branches, ";"); for (i = 1; i <= nf; i++) if (branches[i] ~ /hetAtr/) break; split($4, ps_values, ";"); print $1, branches[i], ps_values[i] } ' absrel_output_clean.csv > absrel_output_clean_hetAtr
