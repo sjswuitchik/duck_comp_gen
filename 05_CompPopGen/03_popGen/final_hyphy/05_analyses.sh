6d85bad86f25f85f8e3bf554e20fb3f12f25a92e
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-04-06 10:12:53 -0700

    Update and rename 05_analyses.R to 05_analyses.sh

--- 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
@@ -2,3 +2,4 @@
 
 
 # download Entrez ID records from https://www.ncbi.nlm.nih.gov/sites/batchentrez using galGal_protIDs.tsv, and 'Send To' File > GenPept with 'Show GI' option
+./parse_gp.awk sequence.gp > entrezIDs.tsv
