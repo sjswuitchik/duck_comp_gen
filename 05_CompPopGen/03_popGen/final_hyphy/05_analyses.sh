28275b69fe79e9b24682c7dc808edb265922396d
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-04-06 10:33:15 -0700

    Update 05_analyses.sh

--- 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
@@ -3,3 +3,6 @@
 
 # download Entrez ID records from https://www.ncbi.nlm.nih.gov/sites/batchentrez using galGal_protIDs.tsv, and 'Send To' File > GenPept with 'Show GI' option
 ./parse_gp.awk sequence.gp > entrezIDs.tsv
+
+# download the background records from NCBI Protein with same 'Send to' options
+./parse_gp.awk sequence.gp > bg_entrezIDs.tsv
