2fc40c2f313da9a631a5ba2009c7a2afc28795b8
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-04-06 10:47:23 -0700

    Update 05_analyses.sh

--- 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
+++ 05_CompPopGen/03_popGen/final_hyphy/05_analyses.sh
@@ -1,15 +1,16 @@
 # currently a janky mix of manipulation in R and awk
 conda activate r
 
-Rscript _____.R 
+Rscript clean_absrel.R 
 
 awk 'BEGIN { FS = OFS = ","} NR == 1 {print "file,total_branches,sig_branches,hetAtr_gene"; next} { match($0, /hetAtr_[^,]+/); hetAtr_gene = substr($0, RSTART, RLENGTH); print $1, $2, $3, hetAtr_gene}' abs_cleanR.csv > abs_clean_final.csv
 
-Rscript ____.R
+Rscript absrel_ortho_wrangling.R
+
 # download Entrez ID records from https://www.ncbi.nlm.nih.gov/sites/batchentrez using galGal_protIDs.tsv, and 'Send To' File > GenPept with 'Show GI' option
 ./parse_gp.awk sequence.gp > entrezIDs.tsv
 
 # download the background records from NCBI Protein with same 'Send to' options
 ./parse_gp.awk sequence.gp > bg_entrezIDs.tsv
 
-Rscript ____.R
+Rscript go_analyses.R
