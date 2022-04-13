545fd4fc00a4f0fa6fed61b9821b95708e11de4b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-04-13 12:46:45 -0700

    Update run_raxml_prot.sh

--- 04_polytomy_resolution/run_raxml_prot.sh
+++ 04_polytomy_resolution/run_raxml_prot.sh
@@ -3,9 +3,9 @@
 #SBATCH -o out
 #SBATCH -e err
 #SBATCH -p shared
-#SBATCH -n 8
+#SBATCH --exclusive
 #SBATCH -t 06-23:00:00
-#SBATCH --mem=12000
+#SBATCH --mem=0
 
 # submit from /n/holyscratch01/informatics/swuitchik/ducks/polytomy_coding
 
@@ -18,7 +18,3 @@ if [ -f ${file}.raxml.bestTree ]; then
 fi
   raxml-ng --msa $file --model HKY+G4 --prefix $file --all --seed 2 --redo --bs-metric fbp
 done
-
-cat orthos/*.bestTree > prot.tree
-
-cat orthos/*.support > prot.support.tree
