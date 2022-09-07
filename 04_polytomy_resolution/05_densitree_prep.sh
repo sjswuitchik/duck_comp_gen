05e030171ef9b914c183056b6e752252475c2c96
sjswuitchik <sjswuit@g.harvard.edu>
2022-09-07 09:22:28 -0400

    Reorg & deletion of old files

--- 04_polytomy_resolution/05_densitree_prep.sh
+++
@@ -1,7 +0,0 @@
-# in /n/holyscratch01/informatics/swuitchik/ducks/polytomy_cnee/trimmed/finished_fastas/
-
-mkdir densitree_input
-cp final.all.tree densitree_input/
-cd densitree_input/
-
-python3 newick2nexus.py -i final.all.tree -o final.all.tree.nex
