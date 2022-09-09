06d56acd8d4fb18ff31ef462053d2a10b2835439
sjswuitchik <sjswuit@g.harvard.edu>
2022-09-09 17:10:05 -0400

    Removing prelim_hyphy dir

--- 05_CompPopGen/03_popGen/prelim_hyphy/reformat.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-
-data <- read_delim("transGene.txt", delim = '\t', col_names = F) %>%
-  select(X2, X1) %>% 
-  write_delim(., "transGene_final.tsv", delim = '\t', col_names = F)
