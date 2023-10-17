678444e8910dadb5a8332db5f8602921eca9b084
Sara J. Smith <ssmith6@mtroyal.ca>
2023-10-17 12:43:31 -0600

    Update 2023_clean_cds_align.sh

--- 05_CompPopGen/2023_clean_cds_align.sh
+++ 05_CompPopGen/2023_clean_cds_align.sh
@@ -4,6 +4,13 @@
 # in /n/holylfs05/LABS/informatics/Everyone/duck_genomics/align/cds_align
 for file in OG*;
 do
-cp $file/$file.MAFFT.Without_low_SP_Col.With_Names /scratch/swuitchik/duck_aligns
+  cp $file/$file.MAFFT.Without_low_SP_Col.With_Names /scratch/swuitchik/ducks/cds_aligns
 done
 
+cd /scratch/swuitchik/ducks/
+mkdir clean_cds_aligns
+
+conda activate align 
+conda install python=3.9
+
+python3 aln_filter.py -i cds_aligns -o clean_cds_aligns
